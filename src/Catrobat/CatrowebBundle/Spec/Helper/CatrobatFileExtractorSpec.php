0e9a69b4ab8019e998f13cd05670b7c04e480f6c
Christian Hartinger <c.k.hartinger@gmail.com>
2013-09-24 14:12:58 +0200

    fixed class definition error

--- src/Catrobat/CatrowebBundle/Spec/Helper/CatrobatFileExtractorSpec.php
+++ src/Catrobat/CatrowebBundle/Spec/Helper/CatrobatFileExtractorSpec.php
@@ -1,37 +1,36 @@
 <?php
 
-namespace spec\Catrobat\CatrowebBundle\Helper;
+namespace Catrobat\CatrowebBundle\Spec\Helper;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
-use Catrobat\CatrowebBundle\CatrowebBundle;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\File\File;
 
 class CatrobatFileExtractorSpec extends ObjectBehavior
 {
-	function let()
-	{
-		$this->beConstructedWith(__DIR__ . "/../Cache/");
-	}
-	
+    function let()
+    {
+    	$this->beConstructedWith(__DIR__ . "/../Cache/");
+    }
+    
     function it_is_initializable()
     {
-        $this->shouldHaveType('Catrobat\CatrowebBundle\Helper\CatrobatFileExtractor');
+    	$this->shouldHaveType('Catrobat\CatrowebBundle\Helper\CatrobatFileExtractor');
     }
     
     function it_extracts_a_valid_file()
     {
     	$filesystem = new Filesystem();
-    	$valid_catrobat_file = new File(__DIR__ . "/../../../../src/Catrobat/CatrowebBundle/Tests/DataFixtures/CatrobatFiles/scaryghost.catrobat");
+    	$valid_catrobat_file = new File(__DIR__ . "/../../Tests/DataFixtures/CatrobatFiles/scaryghost.catrobat");
     	$path_to_extracted_folder = $this->extract($valid_catrobat_file);
-//    	echo($path_to_extracted_folder);
+    	//    	echo($path_to_extracted_folder);
     	$filesystem->remove($path_to_extracted_folder->getWrappedObject());
     }
     
     function it_throws_an_exception_while_extracting_an_invalid_file()
     {
-    	$invalid_catrobat_file = new File(__DIR__ . "/../../../../src/Catrobat/CatrowebBundle/Tests/DataFixtures/CatrobatFiles/invalid_archive.catrobat");
+    	$invalid_catrobat_file = new File(__DIR__ . "/../../Tests/DataFixtures/CatrobatFiles/invalid_archive.catrobat");
     	$this->shouldThrow('Catrobat\CatrowebBundle\Exceptions\InvalidCatrobatFileException')->duringExtract($invalid_catrobat_file);
     }
 }
