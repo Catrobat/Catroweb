3c710c7699ef13c98b360af42f5e764edd328c22
Christian Hartinger <c.k.hartinger@gmail.com>
2013-10-30 16:57:19 +0100

    project upload features added

--- src/Catrobat/ApiBundle/Controller/ApiController.php
+++ src/Catrobat/ApiBundle/Controller/ApiController.php
@@ -8,34 +8,45 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\Validator;
+use Symfony\Component\Security\Core\SecurityContext;
 use Catrobat\CatrowebBundle\Model\UserManager;
+use Catrobat\CatrowebBundle\Model\Requests\AddProjectRequest;
+use Catrobat\CatrowebBundle\Model\ProjectManager;
 
 class ApiController
 {
     protected $templating;
     protected $user_manager;
     protected $validator;
+    protected $context;
+    protected $project_manager;
     
-    public function __construct(EngineInterface $templating, UserManager $user_manager, Validator $validator)
+    public function __construct(EngineInterface $templating, UserManager $user_manager, Validator $validator, SecurityContext $context, ProjectManager $project_manager)
     {
       $this->templating = $templating;
       $this->user_manager = $user_manager;
       $this->validator = $validator;
+      $this->context = $context;
+      $this->project_manager = $project_manager;
     }
   
     public function checkTokenAction()
     {
-        return $this->templating->renderResponse('CatrobatApiBundle:Api:checkToken.json.twig');
+      return $this->templating->renderResponse('CatrobatApiBundle:Api:checkToken.json.twig');
     }
 
-    public function uploadAction()
+    public function uploadAction(Request $request)
     {
-    	return $this->templating->renderResponse('CatrobatApiBundle:Api:index.json.twig');
+      $add_project_request = new AddProjectRequest($this->context->getToken()->getUser(), $request->files->get(0));
+      
+      $this->project_manager->addProject($add_project_request);
+      
+      $num_files = $this->context->getToken()->getUser()->getUsername(); //$request->request->get('fileChecksum'); //$request->files->count();
+    	return $this->templating->renderResponse('CatrobatApiBundle:Api:index.json.twig', array("msg" => $num_files));
     }
     
     public function loginOrRegisterAction(Request $request)
     {
-      
       $userManager = $this->user_manager;
       $retArray = array();
       $username = $request->request->get('registrationUsername');
