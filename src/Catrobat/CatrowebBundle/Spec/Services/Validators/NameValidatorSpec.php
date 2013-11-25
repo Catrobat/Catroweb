d0540fc671965e642e09214be93cbd69da44a6f0
Gregor Sitter <g.sitter@student.tugraz.at>
2013-11-25 15:31:54 +0100

    added NameValidator specs

---
+++ src/Catrobat/CatrowebBundle/Spec/Services/Validators/NameValidatorSpec.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Catrobat\CatrowebBundle\Spec\Services\Validators;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class NameValidatorSpec extends ObjectBehavior
+{
+  
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Catrobat\CatrowebBundle\Services\Validators\NameValidator');
+    }
+
+    /**
+     * @param \Catrobat\CatrowebBundle\Model\ExtractedCatrobatFile $file
+     */
+    function it_throws_an_exception_if_the_name_is_incorrect($file)
+    {
+      $this->shouldThrow('Catrobat\CatrowebBundle\Exceptions\InvalidCatrobatFileException')->duringValidate($file);
+    }
+    
+    
+}
