4e9e22e285faac3ff22d24fdedef6449557eefa9
Christian Hartinger <c.k.hartinger@gmail.com>
2013-11-25 16:31:56 +0100

    added compilerpass to define validation services

--- src/Catrobat/CatrowebBundle/CatrowebBundle.php
+++ src/Catrobat/CatrowebBundle/CatrowebBundle.php
@@ -3,7 +3,14 @@
 namespace Catrobat\CatrowebBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Catrobat\CatrowebBundle\DependencyInjection\ProjectValidatorCompilerPass;
 
 class CatrowebBundle extends Bundle
 {
+  public function build(ContainerBuilder $container)
+  {
+    parent::build($container);
+    $container->addCompilerPass(new ProjectValidatorCompilerPass());
+  }
 }
