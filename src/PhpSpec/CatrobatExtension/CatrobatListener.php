243a37b3429ab023b2134b744132333444a35991
Christian Hartinger <c.k.hartinger@gmail.com>
2013-11-27 12:08:10 +0100

    phpspec extension: use proper io instead of echo

--- src/PhpSpec/CatrobatExtension/CatrobatListener.php
+++ src/PhpSpec/CatrobatExtension/CatrobatListener.php
@@ -5,18 +5,28 @@ namespace PhpSpec\CatrobatExtension;
 use PhpSpec\Event\SuiteEvent;
 use PhpSpec\Event\SpecificationEvent;
 use Symfony\Component\Filesystem\Filesystem;
+use PhpSpec\Console\IO;
 
 class CatrobatListener implements \Symfony\Component\EventDispatcher\EventSubscriberInterface
 {
-
+  protected $io;
+  
   public function beforeSuite(SuiteEvent $event)
   {
     $fixtures_dir = __DIR__."/../../Catrobat/TestBundle/DataFixtures";
     $fixtures_dir = realpath($fixtures_dir)."/";
     define("__SPEC_FIXTURES_DIR__",$fixtures_dir);
-    define("__SPEC_GENERATED_FIXTURES_DIR__",$fixtures_dir."/GeneratedFixtures/");
-    echo("<info>Using data fixtures in " . __SPEC_FIXTURES_DIR__ . "</info>\n");
-    echo("<info>Using generated data fixtures in " . __SPEC_GENERATED_FIXTURES_DIR__ . "</info>\n");
+    define("__SPEC_GENERATED_FIXTURES_DIR__",$fixtures_dir."GeneratedFixtures/");
+    if ($this->io)
+    {
+      $this->io->writeln("<info>Using data fixtures in " . __SPEC_FIXTURES_DIR__ . "</info>");
+      $this->io->writeln("<info>Using generated data fixtures in " . __SPEC_GENERATED_FIXTURES_DIR__ . "</info>");
+    }
+  }
+  
+  public function setIO(IO $io)
+  {
+    $this->io = $io;
   }
   
   public static function getSubscribedEvents()
