afbea90f92dfe9c42d5687ff8f7892cba46bb9fb
Christian Hartinger <c.k.hartinger@gmail.com>
2013-12-03 16:13:25 +0100

    moved cache spec directory and added constant

--- src/Catrobat/CatrowebBundle/Spec/Services/CatrobatFileExtractorSpec.php
+++ src/Catrobat/CatrowebBundle/Spec/Services/CatrobatFileExtractorSpec.php
@@ -9,35 +9,36 @@ use Symfony\Component\HttpFoundation\File\File;
 
 class CatrobatFileExtractorSpec extends ObjectBehavior
 {
-  
-    function let()
-    {
-    	$this->beConstructedWith(__DIR__ . "/../Cache/");
-    }
-    
-    function it_is_initializable()
-    {
-    	$this->shouldHaveType('Catrobat\CatrowebBundle\Services\CatrobatFileExtractor');
-    }
-    
-    function it_throws_an_exception_if_given_an_valid_extraction_directory()
-    {
-      $this->shouldThrow('Catrobat\CatrowebBundle\Exceptions\InvalidStorageDirectoryException')->during('__construct', array(__DIR__ . "/invalid_directory/"));
-    }
-    
-    function it_extracts_a_valid_file()
-    {
-    	$filesystem = new Filesystem();
-    	$valid_catrobat_file = new File(__SPEC_FIXTURES_DIR__."/compass.catrobat");
-    	$extracted_file = $this->extract($valid_catrobat_file);
-    	$extracted_file->shouldHaveType('Catrobat\CatrowebBundle\Model\ExtractedCatrobatFile');
-    	//    	echo($path_to_extracted_folder);
-    	$filesystem->remove($extracted_file->getWrappedObject()->getPath());
-    }
-    
-    function it_throws_an_exception_while_extracting_an_invalid_file()
-    {
-    	$invalid_catrobat_file = new File(__SPEC_FIXTURES_DIR__."/invalid_archive.catrobat");
-    	$this->shouldThrow('Catrobat\CatrowebBundle\Exceptions\InvalidCatrobatFileException')->duringExtract($invalid_catrobat_file);
-    }
+
+  function let()
+  {
+    $this->beConstructedWith(__SPEC_CACHE_DIR__);
+  }
+
+  function it_is_initializable()
+  {
+    $this->shouldHaveType('Catrobat\CatrowebBundle\Services\CatrobatFileExtractor');
+  }
+
+  function it_throws_an_exception_if_given_an_valid_extraction_directory()
+  {
+    $this->shouldThrow('Catrobat\CatrowebBundle\Exceptions\InvalidStorageDirectoryException')->during('__construct', array (
+        __DIR__ . "/invalid_directory/" 
+    ));
+  }
+
+  function it_extracts_a_valid_file()
+  {
+    $filesystem = new Filesystem();
+    $valid_catrobat_file = new File(__SPEC_FIXTURES_DIR__ . "/compass.catrobat");
+    $extracted_file = $this->extract($valid_catrobat_file);
+    $extracted_file->shouldHaveType('Catrobat\CatrowebBundle\Model\ExtractedCatrobatFile');
+  }
+
+  function it_throws_an_exception_while_extracting_an_invalid_file()
+  {
+    $invalid_catrobat_file = new File(__SPEC_FIXTURES_DIR__ . "/invalid_archive.catrobat");
+    $this->shouldThrow('Catrobat\CatrowebBundle\Exceptions\InvalidCatrobatFileException')->duringExtract($invalid_catrobat_file);
+  }
+
 }
--- src/PhpSpec/CatrobatExtension/CatrobatListener.php
+++ src/PhpSpec/CatrobatExtension/CatrobatListener.php
@@ -6,10 +6,19 @@ use PhpSpec\Event\SuiteEvent;
 use PhpSpec\Event\SpecificationEvent;
 use Symfony\Component\Filesystem\Filesystem;
 use PhpSpec\Console\IO;
+use PhpSpec\Event\ExampleEvent;
+use Symfony\Component\Finder\Finder;
 
 class CatrobatListener implements \Symfony\Component\EventDispatcher\EventSubscriberInterface
 {
   protected $io;
+  protected $cache_dir;
+  
+  public function __construct()
+  {
+    $this->cache_dir = realpath(__DIR__."/../../Catrobat/TestBundle/Cache")."/";
+    define("__SPEC_CACHE_DIR__",$this->cache_dir);
+  }
   
   public function beforeSuite(SuiteEvent $event)
   {
@@ -21,12 +30,30 @@ class CatrobatListener implements \Symfony\Component\EventDispatcher\EventSubscr
     {
       $this->io->writeln("<info>Using data fixtures in " . __SPEC_FIXTURES_DIR__ . "</info>");
       $this->io->writeln("<info>Using generated data fixtures in " . __SPEC_GENERATED_FIXTURES_DIR__ . "</info>");
+      $this->io->writeln("<info>Clearing Cache ".__SPEC_CACHE_DIR__." after every example</info>");
     }
+  }
+
+  public function beforeExample(ExampleEvent $event)
+  {
+    $this->emptyDirectory($this->cache_dir);
   }
   
   public function setIO(IO $io)
   {
     $this->io = $io;
+  }
+  
+  private function emptyDirectory($directory)
+  {
+    $filesystem = new Filesystem();
+     
+    $finder = new Finder();
+    $finder->in($directory)->depth(0);
+    foreach ($finder as $file)
+    {
+      $filesystem->remove($file);
+    }
   }
   
   public static function getSubscribedEvents()
@@ -35,7 +62,11 @@ class CatrobatListener implements \Symfony\Component\EventDispatcher\EventSubscr
         'beforeSuite' => array (
             'beforeSuite',
             - 10
-        )
+        ),
+        'beforeExample' => array (
+            'beforeExample',
+            - 10
+        ),
     );
   }
 
