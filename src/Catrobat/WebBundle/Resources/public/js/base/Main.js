ced20fd66cf4514ca60374599dac26cbc9b23e30
Marko Burazer <burazer1988@gmail.com>
2014-08-22 00:34:25 +0200

    WEB-167 + translation

--- src/Catrobat/WebBundle/Resources/public/js/base/Main.js
+++ src/Catrobat/WebBundle/Resources/public/js/base/Main.js
@@ -2,21 +2,30 @@ var Main = function () {
   var self = this;
 
   $(window).ready(function() {
-    self.setFooterViewListener();
-    self.setHeaderMobileSearchbarListener();
+    self.setClickListener();
+    self.setWindowResizeListener();
   });
 
-  self.setFooterViewListener = function() {
+  self.setClickListener = function() {
     $('#footer-more-less').click(function () {
       $('body').toggleClass('footer-expand');
       $(window).scrollTop($(document).height());
     });
-  };
 
-  self.setHeaderMobileSearchbarListener = function() {
     $('#menu-mobile').find('.btn-search').click(function() {
       $('nav').toggleClass('searchbar-visible');
       $('nav').find('input').focus();
+    });
+
+    $('.show-nav-dropdown').click(function() {
+      //todo: dropdown is at correct position after 3x clicking!!!
+      $('#nav-dropdown').css('left', $('nav').position().left + $('nav').outerWidth() - $('#nav-dropdown').width()).toggle();
+    });
+  };
+
+  self.setWindowResizeListener = function() {
+    $(window).resize(function() {
+      $('#nav-dropdown').hide();
     });
   };
 };
