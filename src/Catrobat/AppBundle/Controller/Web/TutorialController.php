0facda7584753d91b5265d53bcadd3ee488196e5
Patrick Trummer <patricktrummer88@gmail.com>
2014-12-09 15:49:02 +0100

    finalized help page

--- src/Catrobat/AppBundle/Controller/Web/TutorialController.php
+++ src/Catrobat/AppBundle/Controller/Web/TutorialController.php
@@ -10,38 +10,48 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 class TutorialController extends Controller
 {
   /**
-   * @Route("/tutorial", name="catrobat_web_tutorial")
+   * @Route("/help", name="catrobat_web_help")
    * @Method({"GET"})
    */
-  public function tutorialAction()
+  public function helpAction()
   {
-    return $this->get("templating")->renderResponse(':tutorials:tutorial.html.twig');
+    return $this->get("templating")->renderResponse(':help:help.html.twig');
   }
 
   /**
-   * @Route("/tutorial/tutorials", name="catrobat_web_tutorials")
+   * @Route("/hour-of-code/{page}", name="catrobat_web_hour_of_code", defaults={"page" = 0}, requirements={"page":"\d+"})
+   * @Route("/hourOfCode/{page}", name="catrobat_web_hourOfCode", defaults={"page" = 0}, requirements={"page":"\d+"})
    * @Method({"GET"})
    */
-  public function tutorialsAction()
+  public function hourOfCodeAction($page)
   {
-    return $this->get("templating")->renderResponse(':tutorials:tutorials.html.twig');
+    return $this->get("templating")->renderResponse(':help:hourOfCode.html.twig');
   }
 
-  /**
-   * @Route("/tutorial/stepByStep", name="catrobat_web_stepByStep")
+    /**
+   * @Route("/step-by-step", name="catrobat_web_stepByStep")
    * @Method({"GET"})
    */
   public function stepByStepAction()
   {
-    return $this->get("templating")->renderResponse(':tutorials:stepByStep.html.twig');
+    return $this->get("templating")->renderResponse(':help:stepByStep.html.twig');
   }
 
   /**
-   * @Route("/tutorial/starterPrograms", name="catrobat_web_starter")
+   * @Route("/tutorials", name="catrobat_web_tutorials")
+   * @Method({"GET"})
+   */
+  public function tutorialsAction()
+  {
+      return $this->get("templating")->renderResponse(':help:tutorials.html.twig');
+  }
+
+    /**
+   * @Route("/starter-programs", name="catrobat_web_starter")
    * @Method({"GET"})
    */
   public function starterProgramsAction()
   {
-    return $this->get("templating")->renderResponse(':tutorials:starterPrograms.html.twig');
+    return $this->get("templating")->renderResponse(':help:starterPrograms.html.twig');
   }
 }
