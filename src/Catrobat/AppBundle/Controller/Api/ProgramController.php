69691a6f32b5ca9feb84a1495f6695167806457c
Christian Hartinger <c.k.hartinger@gmail.com>
2015-02-10 16:33:50 +0100

    added get program info by id to api

---
+++ src/Catrobat/AppBundle/Controller/Api/ProgramController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Catrobat\AppBundle\Controller\Api;
+
+use Catrobat\AppBundle\Services\ElapsedTimeString;
+use Catrobat\AppBundle\Model\ProgramManager;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Catrobat\AppBundle\Services\ScreenshotRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class ProgramController extends Controller
+{
+
+  /**
+   * @Route("/api/projects/getInfoById.json", name="catrobat_api_info_by_id", defaults={"_format": "json"})
+   * @Method({"GET"})
+   */
+  public function showProgramAction(Request $request)
+  {
+    $id = intval($request->query->get('id'));
+    $program_manager = $this->get("programmanager");
+    $screenshot_repository = $this->get("screenshotrepository");
+    $elapsed_time = $this->get("elapsedtime");
+    
+    $retArray = array ();
+    
+    $programs = array();
+    $program = $program_manager->find($id);
+    if ($program == null)
+    {
+        return JsonResponse::create(array("Error" => "Project not found (uploaded)", "preHeaderMessages" => ""));
+    }
+    else
+    {
+        $numbOfTotalProjects = 1;
+        $programs[] = $program;
+    }
+    
+    $retArray['CatrobatProjects'] = array ();
+    foreach($programs as $program)
+    {
+      $new_program = array ();
+      $new_program['ProjectId'] = $program->getId();
+      $new_program['ProjectName'] = $program->getName();
+      $new_program['ProjectNameShort'] = $program->getName();
+      $new_program['Author'] = $program->getUser()->getUserName();
+      $new_program['Description'] = $program->getDescription();
+      $new_program['Version'] = $program->getCatrobatVersionName();
+      $new_program['Views'] = $program->getViews();
+      $new_program['Downloads'] = $program->getDownloads();
+      $new_program['Uploaded'] = $program->getUploadedAt()->getTimestamp();
+      $new_program['UploadedString'] = $elapsed_time->getElapsedTime($program->getUploadedAt()->getTimestamp());
+      $new_program['ScreenshotBig'] = $screenshot_repository->getScreenshotWebPath($program->getId());
+      $new_program['ScreenshotSmall'] = $screenshot_repository->getThumbnailWebPath($program->getId());
+      $new_program['ProjectUrl'] = "details/" . $program->getId();
+      $new_program['DownloadUrl'] = "download/" . $program->getId() . ".catrobat";
+      $retArray['CatrobatProjects'][] = $new_program;
+    }
+    $retArray['completeTerm'] = "";
+    $retArray['preHeaderMessages'] = "";
+    
+    $retArray['CatrobatInformation'] = array (
+        "BaseUrl" => ($request->isSecure() ? 'https://' : 'http://'). $request->getHttpHost() . $request->getBaseUrl() . '/',
+        "TotalProjects" => $numbOfTotalProjects,
+        "ProjectsExtension" => ".catrobat" 
+    );
+    
+    return JsonResponse::create($retArray);
+  }
+
+}
---
+++ src/Catrobat/AppBundle/Features/Api/program_details.feature
@@ -0,0 +1,58 @@
+@api
+Feature: Get details for a specific program
+  Background: 
+    Given there are users:
+      | name     | password | token      |
+      | Catrobat | 12345    | cccccccccc |
+      | User1    | vwxyz    | aaaaaaaaaa |
+    And there are programs:
+      | id | name      | description | owned by | downloads | views | upload time      | version |
+      | 1  | program 1 | p1          | Catrobat | 3         | 12    | 01.01.2013 12:00 | 0.8.5   |
+      | 2  | program 2 |             | Catrobat | 333       | 9     | 22.04.2014 13:00 | 0.8.5   |
+      | 3  | program 3 |             | User1    | 133       | 33    | 01.01.2012 13:00 | 0.8.5   |
+    And the current time is "01.08.2014 13:00"
+
+      
+  Scenario: show details of a program with given id
+    Given I have a parameter "id" with value "2"
+    When I GET "/api/projects/getInfoById.json" with these parameters
+    Then I should get the json object:
+      """
+      {
+          "completeTerm":"",
+          "CatrobatInformation": {
+                                   "BaseUrl":"http://localhost/",
+                                   "TotalProjects":1,
+                                   "ProjectsExtension":".catrobat"
+                                  },
+          "CatrobatProjects":[{
+                                "ProjectId": 2,
+                                "ProjectName":"program 2",
+                                "ProjectNameShort":"program 2",
+                                "ScreenshotBig":"images/default/screenshot.png",
+                                "ScreenshotSmall":"images/default/thumbnail.png",
+                                "Author":"Catrobat",
+                                "Description":"",
+                                "Uploaded": 1398171600,
+                                "UploadedString":"3 months ago",
+                                "Version":"0.8.5",
+                                "Views":"9",
+                                "Downloads":"333",
+                                "ProjectUrl":"details/2",
+                                "DownloadUrl":"download/2.catrobat"
+                            }],
+          "preHeaderMessages":""
+      }
+      """
+      
+  Scenario: return error if no program matches the given id
+    Given I have a parameter "id" with value "9"
+    When I GET "/api/projects/getInfoById.json" with these parameters
+    Then I should get the json object:
+      """
+        {
+            "Error": "Project not found (uploaded)",
+            "preHeaderMessages": ""
+        }
+      """
+      
