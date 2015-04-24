7500e8a0386b5583a1ca45d3dc35f9d165d0b3cf
Christian Hartinger <c.k.hartinger@gmail.com>
2015-04-24 15:12:01 +0200

    modified deploy config for multistage

--- app/config/deploy.dist.rb
+++ app/config/deploy.dist.rb
@@ -3,42 +3,33 @@ set :ssh_options, {
     forward_agent: true
 }
 
-#Server settings
-set :application, "Pocketcode"
-set :domain,      ""                        #i.e. test.google.com
-set :deploy_to,   ""                        #i.e. /var/www/google
+set :stages,        %w()                  ### Insert your different stages and default here
+set :default_stage, ""                    ### Run cap multistage:prepare to init config files
+set :stage_dir,     "app/config/deploy"
+require 'capistrano/ext/multistage'
+
 set :app_path,    "app"
-set :user, ""                               #ftp/ssh-user
+set :use_sudo,    false
 
-set :use_sudo, false
-
-#Deploy settings
+# Deploy settings
 set :use_composer, true
-# set :update_vendors, true
 set :symfony_env_prod, "prod"
 set :model_manager, "doctrine"
 set :dump_assetic_assets, true
 
-
-#files which are not in the git repo. When you add one, upload it yourself via ftp first
+# files which are not in the git repo. When you add one, upload it yourself via ftp first
 set :shared_files,      ["app/config/parameters.yml"]
-set :shared_children,     [app_path + "/logs", web_path + "/resources", "vendor"] #uploads could be the projects folder
+set :shared_children,   [app_path + "/logs", web_path + "/resources", "vendor", "backups"]
 
-#Which repo to use and how
-set :repository,  "/home/catroweb/projects/catroweb/"        #It's the path to your local repo
+# Which repo to use and how
+set :repository,  "."                  # It's the path to your local repo
 set :scm,         :git
 set :deploy_via, :rsync_with_remote_cache
 set :keep_releases,  3
 
-role :web,        domain                         # Your HTTP server, Apache/etc
-role :app,        domain, :primary => true       # This may be the same as your `Web` server
-
-
-#shitty permissions...
+# permissions
 set :writable_dirs,       ["app/cache", "app/logs", "web/resources/featured", "web/resources/extract", "web/resources/programs", "web/resources/screenshots", "web/resources/thumbnails", "web/resources/apk"]
 set :webserver_user,      "www-data"
 set :permission_method,   :acl
 set :use_set_permissions, true
 
-# for logging
-#logger.level = Logger::MAX_LEVEL
