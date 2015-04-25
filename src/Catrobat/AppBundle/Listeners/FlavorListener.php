bd956b459b2178f5b8739f2c993a28fd8984eff8
Christian Hartinger <c.k.hartinger@gmail.com>
2015-04-25 15:06:40 +0200

    inject current flavor to router

--- src/Catrobat/AppBundle/Listeners/FlavorListener.php
+++ src/Catrobat/AppBundle/Listeners/FlavorListener.php
@@ -5,9 +5,17 @@ namespace Catrobat\AppBundle\Listeners;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\Routing\RouterInterface;
 
 class FlavorListener
 {
+    private $router;
+    
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+    
     public function onKernelRequest(GetResponseEvent $event)
     {
         $attributes = $event->getRequest()->attributes;
@@ -27,6 +35,12 @@ class FlavorListener
                 $attributes->set('flavor','pocketcode');
                 $session->set('flavor','pocketcode');
             }
+        }
+        
+        $context = $this->router->getContext();
+        if (!$context->hasParameter('flavor'))
+        {
+            $context->setParameter('flavor', $attributes->get('flavor'));
         }
     }
 }
