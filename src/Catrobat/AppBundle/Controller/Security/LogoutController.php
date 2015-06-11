11db1e182985efc8df60db1790a7ab911221dde7
Christian Hartinger <c.k.hartinger@gmail.com>
2015-06-11 13:27:09 +0200

    custom logout

---
+++ src/Catrobat/AppBundle/Controller/Security/LogoutController.php
@@ -0,0 +1,12 @@
+<?php
+namespace Catrobat\AppBundle\Controller\Security;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class LogoutController extends Controller
+{
+    public function logoutAction() {
+        $this->get('security.context')->setToken(null);
+        return $this->redirect($this->generateUrl('index'));
+    }
+}
