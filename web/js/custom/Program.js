010c35a05249e2481ccd1bc4b839a821ea6d7270
Gregor Sitter <w00ty2k1@gmail.com>
2015-06-12 15:46:17 +0200

    apk popup

--- web/js/custom/Program.js
+++ web/js/custom/Program.js
@@ -1,9 +1,12 @@
-var Program = function(status_url, create_url)
+var Program = function(status_url, create_url, apk_preparing, apk_text, waiting_gif)
 {
     var self = this;
     
     self.status_url = status_url;
     self.create_url = create_url;
+    self.apk_preparing = apk_preparing;
+    self.apk_text = apk_text;
+    self.waiting_gif = waiting_gif;
     
     self.getApkStatus = function()
     {
@@ -15,6 +18,7 @@ var Program = function(status_url, create_url)
         $('.btn-apk').hide();
         $('#apk-pending').show().css("display", "inline-block");
         $.get(self.create_url, null, self.onResult);
+        self.showPopup();
     };
 
     self.onResult = function(data)
@@ -59,6 +63,51 @@ var Program = function(status_url, create_url)
           }).fail(function() {
               alert('ERROR'); // display better error message
           });
+        });
+    };
+
+    self.showPopup = function() {
+        var popup_div = $('<div></div>');
+        var dark_background = $('<div id="bg-dark"></div>');
+        dark_background.css({
+            'position': 'fixed',
+            'width': '100%',
+            'height': '100%',
+            'background-color': 'black',
+            'left': '0',
+            'top': '0',
+            'opacity': '0.5'
+        });
+
+        popup_div.css({
+            'position': 'fixed',
+            'width': '320px',
+            'height': '250px',
+            'background-color': '#05222a',
+            'left': '50%',
+            'top': '50%',
+            'border': '3px solid #17A5B8',
+            'border-radius': '10px',
+            'padding': '20px 5px 20px 5px',
+            'margin': '-160px 0 0 -210px',
+            'text-align': 'center',
+            'font-size': '15px'
+        });
+
+        popup_div.append("<h2>" + self.apk_preparing + " <span class='blink-one'>.</span> <span class='blink-two'>.</span> <span class='blink-three'>.</span> </h2><br>");
+        popup_div.append('<img class="pending-icon" src="' + waiting_gif + '">');
+        popup_div.append("<p>" + self.apk_text + "</p>");
+
+        var ok_button = '<button id="ok-button" class="btn btn-primary" style="min-width: 50%; margin-top: 2px">OK</button>';
+        popup_div.append(ok_button);
+
+        $('body').append(dark_background);
+        $('body').append(popup_div);
+
+        $('#bg-dark, #ok-button').click(function() {
+            popup_div.remove();
+            dark_background.remove();
+            ok_button.remove();
         });
     };
     
