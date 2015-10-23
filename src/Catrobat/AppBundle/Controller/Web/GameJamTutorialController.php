537ef544854524b38b338057175c4e7274352ed7
Gregor Sitter <g.sitter@student.tugraz.at>
2015-10-23 17:22:21 +0200

    WEB-273 added first card for testing

--- src/Catrobat/AppBundle/Controller/Web/GameJamTutorialController.php
+++ src/Catrobat/AppBundle/Controller/Web/GameJamTutorialController.php
@@ -28,74 +28,55 @@ class GameJamTutorialController extends Controller
       }
 
       $blocks = array();
-      $blocks[0] = array('block' => 'one', 'images' => 1);
-      $blocks[1] = array('block' => 'two', 'images' => 1);
+      $blocks[0] = array('image_orientation' => "landscape", 'image_count' => 2);
+      $blocks[1] = array('video_link' => "6ytY_vfsnNU");
+      $blocks[2] = array('program_id' => "0");
 
-      switch($page) {
-        case 2:
-        case 3:
-        case 10:
-          $blocks[2] = array('block' => 'three', 'images' => 1);
-          break;
-
-        case 9:
-          $blocks[0] = array('block' => 'one', 'images' => 2);
-          $blocks[2] = array('block' => 'three', 'images' => 1);
-          break;
-
-        case 11:
-          $blocks[1] = array('block' => 'two', 'images' => 2);
-          break;
-
-        case 12:
-          $blocks[1] = array('block' => 'two', 'images' => 3);
-          break;
-      }
-
-      $example_link = null;
       switch($page) {
         case 1:
-          $example_link = $this->generateUrl('program', array('id' => 3983));
-          break;
+            $blocks[0] = array('image_orientation' => "landscape", 'image_count' => 2);
+            $blocks[1] = array('video_link' => "6ytY_vfsnNU");
+            $blocks[2] = array('program_id' => "0");
+            break;
         case 2:
-          $example_link = $this->generateUrl('program', array('id' => 3984));
-          break;
+            $blocks[0] = array('image_orientation' => "potrait", 'image_count' => 2);
+            $blocks[1] = array('video_link' => "6ytY_vfsnNU");
+            $blocks[2] = array('program_id' => "0");
+            break;
         case 3:
-          $example_link = $this->generateUrl('program', array('id' => 3985));
-          break;
+            $blocks[0] = array('image_orientation' => "landscape", 'image_count' => 3);
+            $blocks[1] = array('video_link' => "6ytY_vfsnNU");
+            $blocks[2] = array('program_id' => "0");
+            break;
         case 4:
-          $example_link = $this->generateUrl('program', array('id' => 3986));
+            $blocks[0] = array('image_orientation' => "landscape", 'image_count' => 3);
+            $blocks[1] = array('video_link' => "6ytY_vfsnNU");
+            $blocks[2] = array('program_id' => "0");
           break;
         case 5:
-          $example_link = $this->generateUrl('program', array('id' => 3987));
+            $blocks[0] = array('image_orientation' => "potrait", 'image_count' => 2);
+            $blocks[1] = array('video_link' => "6ytY_vfsnNU");
+            $blocks[2] = array('program_id' => "0");
           break;
-        case 6:
-          $example_link = $this->generateUrl('program', array('id' => 3988));
-          break;
-        case 7:
-          $example_link = $this->generateUrl('program', array('id' => 3990));
-          break;
-        case 8:
-          $example_link = $this->generateUrl('program', array('id' => 3991));
-          break;
-        case 9:
-          $example_link = $this->generateUrl('program', array('id' => 3992));
-          break;
-        case 10:
-          $example_link = $this->generateUrl('program', array('id' => 3979));
-          break;
-        case 11:
-          $example_link = $this->generateUrl('program', array('id' => 3981));
-          break;
-        case 12:
-          $example_link = $this->generateUrl('program', array('id' => 3982));
-          break;
+//
+//        case 9:
+//          $blocks[0] = array('block' => 'one', 'images' => 2);
+//          $blocks[2] = array('block' => 'three', 'images' => 1);
+//          break;
+//
+//        case 11:
+//          $blocks[1] = array('block' => 'two', 'images' => 2);
+//          break;
+//
+//        case 12:
+//          $blocks[1] = array('block' => 'two', 'images' => 3);
+//          break;
       }
+
 
       return $this->get('templating')->renderResponse(':help:gamejamtutorialcard.html.twig', array(
         'page' => $page,
         'blocks' => $blocks,
-        'example_link' => $example_link
       ));
   }
 }
