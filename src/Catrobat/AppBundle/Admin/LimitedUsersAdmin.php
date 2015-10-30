70f5ac8624be96d1920f14c46d58b01a4a910751
Christian Hartinger <c.k.hartinger@gmail.com>
2015-10-30 17:13:24 +0100

    Added admin interface for limited accounts

---
+++ src/Catrobat/AppBundle/Admin/LimitedUsersAdmin.php
@@ -0,0 +1,39 @@
+<?php
+namespace Catrobat\AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+
+class LimitedUsersAdmin extends Admin
+{
+    protected $baseRouteName = 'admin_limited_users';
+    protected $baseRoutePattern = 'limited_users';
+    
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+        ->addIdentifier('id')
+        ->add('username')
+        ->add('email')
+        ->add('limited', 'boolean', array('editable' => true))
+        ;
+    }
+    
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+        ->add('username')
+        ->add('email')
+        ->add('limited')
+        ;
+    }
+    
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        $collection->clearExcept(array('list','edit'));
+    }
+}
