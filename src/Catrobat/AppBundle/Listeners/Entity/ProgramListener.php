5746278fdd77c2dc864a2595109060c2c3dd57ad
claudiokirchmair <claudio.kirchmair@student.tugraz.at>
2016-02-24 13:48:49 +0100

    Added behat tests. Refactored ProgramListener.

--- src/Catrobat/AppBundle/Listeners/Entity/ProgramListener.php
+++ src/Catrobat/AppBundle/Listeners/Entity/ProgramListener.php
@@ -5,18 +5,19 @@ namespace Catrobat\AppBundle\Listeners\Entity;
 
 use Catrobat\AppBundle\Entity\Program;
 use Catrobat\AppBundle\Services\FacebookPostService;
-use Doctrine\ORM\Event\LifecycleEventArgs;
+use Catrobat\AppBundle\StatusCode;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
 
 class ProgramListener
 {
     private $facebook_post_service;
 
-    public function __construct(FacebookPostService $facebook_post_service)
+    public function __construct($facebook_post_service)
     {
         $this->facebook_post_service = $facebook_post_service;
     }
 
-    public function preUpdate(Program $program, LifecycleEventArgs $eventArgs)
+    public function preUpdate(Program $program, PreUpdateEventArgs $eventArgs)
     {
         $fb_post_id = $program->getFbPostId();
 
@@ -25,8 +26,10 @@ class ProgramListener
             $status_code = $this->facebook_post_service->removeFbPost($fb_post_id);
 
             if ($status_code != StatusCode::FB_DELETE_ERROR) {
-                $eventArgs->setNewValue('fb_post_id', '');
-                $eventArgs->setNewValue('fb_post_url', '');
+                $eventArgs->getEntity()->setFbPostId('');
+                $eventArgs->getEntity()->setFbPostUrl('');
+                $eventArgs->getEntityManager()->getUnitOfWork()->recomputeSingleEntityChangeSet($eventArgs->getEntityManager()->getClassMetadata('Catrobat\AppBundle\Entity\Program'), $eventArgs->getEntity());
+
             }
         }
     }
