aa98a7349996a3a7252b0d307ee728b96ef0f6fb
Wolfgang Karl <w.karl@student.tugraz.at>
2016-04-11 07:12:34 +0200

    WEB-270 fix broken controller

--- src/Catrobat/AppBundle/Controller/Web/TeacherDownloadAreaController.php
+++ src/Catrobat/AppBundle/Controller/Web/TeacherDownloadAreaController.php
@@ -1,3 +1,24 @@
+<?php
+
+namespace Catrobat\AppBundle\Controller\Web;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Wolfgang Karl
+ * Date: 20.03.16
+ * Time: 14:55
+ */
+class TeacherDownloadAreaController extends Controller
+{
+    const PASSWORD = "1234";
+    /**
      * @Route("/teachersLogin", name="teachersLogin")
      * @Method({"GET"})
      */
