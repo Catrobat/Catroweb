88bdef53838f34557e02ae3a7f17cb66bcf3dabc
woka1337 <w.karl@student.tugraz.at>
2016-04-11 06:46:46 +0200

    WEB-270 basic implementation

--- src/Catrobat/AppBundle/Controller/Web/TeacherDownloadAreaController.php
+++ src/Catrobat/AppBundle/Controller/Web/TeacherDownloadAreaController.php
@@ -25,7 +25,35 @@ class TeacherDownloadAreaController extends Controller
      */
     public function teachersLoginAction(Request $request)
     {
+        if($this->isAuthenticatedAsTeacher()) {
+            return $this->redirectToRoute('teachers');
+        }
         return $this->get('templating')->renderResponse(':teachers:teachersLogin.html.twig');
+    }
+
+    /**
+     * @Route("/teachersLogout", name="teachersLogout")
+     * @Method({"GET"})
+     */
+    public function teachersLogoutAction(Request $request)
+    {
+        $session = $this->get('session');
+        $session->set("isAuthenticatedAsTeacher", 0);
+        return $this->redirectToRoute('teachersLogin');
+    }
+
+    /**
+     * @Route("/teachersAuth", name="teachersAuth")
+     * @Method({"POST"})
+     */
+    public function teachersLoginPostAction(Request $request)
+    {
+        $password = $request->get("password");
+        if(self::PASSWORD === $password) {
+            self::authenticateAsTeacher();
+            return $this->redirectToRoute('teachers');
+        }
+        return $this->redirectToRoute('teachersLogin');
     }
 
     /**
@@ -39,7 +67,65 @@ class TeacherDownloadAreaController extends Controller
             return $this->redirectToRoute('teachersLogin'); // fos_user_security_login
         }
 
-        return new Response("hello world! :)");
+        return $this->get('templating')->renderResponse(':teachers:teachers.html.twig');
+    }
+
+    /**
+     * @Route("/teachersDownload", name="teachersDownload")
+     * @Method({"GET"})
+     */
+    public function teachersDownloadAction(Request $request)
+    {
+        $file = $this->get('kernel')->getRootDir()."/../web/resources/teachers/templates.zip";
+
+        if (file_exists($file)) {
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/zip');
+            header('Content-Disposition: attachment; filename="'.basename($file).'"');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($file));
+            readfile($file);
+            exit;
+        } else {
+            return new Response("No template has been uploaded so far.");
+        }
+    }
+
+    /**
+     * @Route("/teachersTemplateUpload", name="teachersTemplateUpload")
+     * @Method({"GET"})
+     */
+    public function teachersUploadAction(Request $request)
+    {
+        if($this->isAuthenticatedAsTeacher() === false)
+        {
+            return $this->redirectToRoute('teachersLogin'); // fos_user_security_login
+        }
+
+        return $this->get('templating')->renderResponse(':teachers:teachersUpload.html.twig');
+    }
+
+    /**
+     * @Route("/teachersTemplatePostUpload", name="teachersTemplatePostUpload")
+     * @Method({"POST"})
+     */
+    public function teachersUploadPostAction(Request $request)
+    {
+        if($this->isAuthenticatedAsTeacher() === false)
+        {
+            return $this->redirectToRoute('teachersLogin'); // fos_user_security_login
+        }
+
+        $file = $this->get('kernel')->getRootDir()."/../web/resources/teachers/templates.zip";
+
+        if (move_uploaded_file($_FILES['templates']['tmp_name'], $file)) {
+            return new Response("Templates have been uploaded successfully!");
+        } else {
+            return new Response("Failed to upload templates!");
+        }
+
     }
 
     public function isAuthenticatedAsTeacher() {
