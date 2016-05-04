66d2c23aedf6468f1282e01966750ce831440acf
claudiokirchmair <claudio.kirchmair@student.tugraz.at>
2016-05-04 14:29:12 +0200

    WEB-252 Added exception handling and logging to preUpdate ProgramListener

--- src/Catrobat/AppBundle/Listeners/Entity/ProgramListener.php
+++ src/Catrobat/AppBundle/Listeners/Entity/ProgramListener.php
@@ -11,10 +11,12 @@ use Doctrine\ORM\Event\PreUpdateEventArgs;
 class ProgramListener
 {
     private $facebook_post_service;
+    private $logger;
 
-    public function __construct($facebook_post_service)
+    public function __construct($facebook_post_service, Logger $logger)
     {
         $this->facebook_post_service = $facebook_post_service;
+        $this->logger = $logger;
     }
 
     public function preUpdate(Program $program, PreUpdateEventArgs $eventArgs)
@@ -23,13 +25,17 @@ class ProgramListener
 
         if ($eventArgs->hasChangedField('visible') && !$eventArgs->getNewValue('visible') &&
             $fb_post_id != null && $fb_post_id != '') {
-            $status_code = $this->facebook_post_service->removeFbPost($fb_post_id);
 
-            if ($status_code != StatusCode::FB_DELETE_ERROR) {
-                $eventArgs->getEntity()->setFbPostId('');
-                $eventArgs->getEntity()->setFbPostUrl('');
-                $eventArgs->getEntityManager()->getUnitOfWork()->recomputeSingleEntityChangeSet($eventArgs->getEntityManager()->getClassMetadata('Catrobat\AppBundle\Entity\Program'), $eventArgs->getEntity());
+            try {
+                $status_code = $this->facebook_post_service->removeFbPost($fb_post_id);
 
+                if ($status_code != StatusCode::FB_DELETE_ERROR) {
+                    $eventArgs->getEntity()->setFbPostId('');
+                    $eventArgs->getEntity()->setFbPostUrl('');
+                    $eventArgs->getEntityManager()->getUnitOfWork()->recomputeSingleEntityChangeSet($eventArgs->getEntityManager()->getClassMetadata('Catrobat\AppBundle\Entity\Program'), $eventArgs->getEntity());
+                }
+            } catch (Exception $e) {
+                $this->logger->error('ProgramListener->preUpdate: FbPostId[' . $fb_post_id . '], Message[' . $e->getMessage() . ']');
             }
         }
     }
