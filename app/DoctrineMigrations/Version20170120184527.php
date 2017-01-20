989540634ba7d283b891c13400640768cb12732d
Andrej Knaus <andrej.knaus@student.tugraz.at>
2017-01-20 20:19:51 +0100

    changed migration name

--- app/DoctrineMigrations/Version20170120184527.php
+++ app/DoctrineMigrations/Version20170120184527.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20170110184527 extends AbstractMigration
+class Version20170120184527 extends AbstractMigration
 {
     /**
      * @param Schema $schema
