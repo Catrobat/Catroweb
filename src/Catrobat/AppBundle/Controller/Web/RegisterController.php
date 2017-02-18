88784cf60819c056d60e502ceab6e8713dcaf879
Kevin Haslinger <kevin.haslinger@student.tugraz.at>
2017-02-18 18:12:13 +0100

    WEB-328_NoMailForUsername dont allow email as username this also adds error messages for passwords dont match on register page and adds a controller between the used middleware and the view
    change routing to have a register controller before accessing foreign middleware
    
    add error message for username containing mailaddress
    
    add error message for passwords didnt match
    
    minor changes
    
    correct route to /register from /register/

---
+++ src/Catrobat/AppBundle/Controller/Web/RegisterController.php
@@ -0,0 +1,59 @@
+<?php
+namespace Catrobat\AppBundle\Controller\Web;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+
+class RegisterController extends Controller
+{
+  /**
+   * @Route("/register", name="register_check")
+   * @Method({"POST"})
+   */
+  public function checkRegistration(Request $request)
+  {
+    $route = 'fos_user_registration_register';
+    $error = false;
+
+    $username = $request->request->get('sonata_user_registration_form')['username'];
+    if (strpos($username, "@") !== false)
+    {
+      $this->addFlash(
+        'catroweb_error_message',
+        "errors.username.not_email"
+      );
+      $error = true;
+    }
+
+    $password = $request->request->get('sonata_user_registration_form')['plainPassword'];
+    if ($password['first'] !== $password['second'])
+    {
+      $this->addFlash(
+        'catroweb_error_message',
+        "passwordsNoMatch"
+      );
+      $error = true;
+    }
+
+    if ($error)
+    {
+      return $this->redirectToRoute('register_form');
+    }
+
+    $response = $this->forward('SonataUserBundle:RegistrationFOSUser1:register');
+    return $response;
+  }
+
+  /**
+   * @Route("/register", name="register_form")
+   * @Method({"GET"})
+   */
+  public function showRegistrationForm(Request $request)
+  {
+    $response = $this->forward('SonataUserBundle:RegistrationFOSUser1:register');
+    return $response;
+  }
+}
+
