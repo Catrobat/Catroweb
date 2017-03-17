32fed3e4d019b5eebd08fc29e8d5b365061c0afb
Kevin Haslinger <kevin.haslinger@student.tugraz.at>
2017-03-17 15:43:51 +0100

    WEB-380 show progress of backup this adds a nice progress bar to the console command (when executing it via the console) and also adds a modal which stops the user from interacting with the website during a backup is in progress
    remove now unneeded files
    
    add progressbar to console command
    
    add nice user response when creating backup over webview

--- src/Catrobat/AppBundle/Commands/CreateBackupCommand.php
+++ src/Catrobat/AppBundle/Commands/CreateBackupCommand.php
@@ -4,10 +4,12 @@ namespace Catrobat\AppBundle\Commands;
 
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Process\Process;
 use Catrobat\AppBundle\Commands\Helpers\CommandHelper;
 
@@ -26,26 +28,44 @@ class CreateBackupCommand extends ContainerAwareCommand
     {
         $this->output = $output;
 
+        $progress = new ProgressBar($this->output, 7);
+        $progress->setFormat(" %current%/%max% [%bar%] %percent:3s%% %elapsed:6s%/%estimated:-6s% %memory:6s% \n %message%");
+        $progress->setMessage('Starting...');
+        $progress->setOverwrite(true);
+        $progress->start();
+
         $backup_dir = realpath($this->getContainer()->getParameter('catrobat.backup.dir'));
-        $output->writeln('Using backup directory '.$backup_dir);
+
+        $progress->setMessage('Using backup directory ' . $backup_dir);
 
         if ($this->getContainer()->getParameter('database_driver') != 'pdo_mysql')
+        {
+            $progress->setMessage('Error: This script only supports mysql databases');
+            $progress->finish();
+
             throw new \Exception('This script only supports mysql databases');
+        }
+        $progress->advance();
 
         if ($input->hasArgument('backupName') && $input->getArgument('backupName') != "") {
             $zip_path = $backup_dir . '/' . $input->getArgument('backupName') . '.tar.gz';
         } else {
             $zip_path = $backup_dir . '/'.date('Y-m-d_His') . '.tar.gz';
         }
+        $progress->advance();
+        $progress->setMessage('Database driver set, Outputpath specified as ' . $zip_path);
 
         $sql_path = tempnam($backup_dir, 'Sql');
         $database_name = $this->getContainer()->getParameter('database_name');
         $database_user = $this->getContainer()->getParameter('database_user');
         $database_password = $this->getContainer()->getParameter('database_password');
-        CommandHelper::executeShellCommand("mysqldump -u $database_user -p$database_password $database_name > $sql_path",
-          array('timeout' => 7200), 'Saving SQL file', $output);
+        $progress->setMessage('Saving SQL file');
 
-        $output->writeln('Creating archive at '.$zip_path);
+        CommandHelper::executeShellCommand("mysqldump -u $database_user -p$database_password $database_name > $sql_path",
+          array('timeout' => 7200));
+
+        $progress->advance();
+        $progress->setMessage('Database dump completed.' . " Creating archive at " . $zip_path);
 
         $thumbnail_dir = $this->getContainer()->getParameter('catrobat.thumbnail.dir');
         $screenshot_dir = $this->getContainer()->getParameter('catrobat.screenshot.dir');
@@ -54,12 +74,22 @@ class CreateBackupCommand extends ContainerAwareCommand
         $mediapackage_dir = $this->getContainer()->getParameter('catrobat.mediapackage.dir');
         $template_dir = $this->getContainer()->getParameter('catrobat.template.dir');
 
+        $progress->advance();
+        $progress->setMessage('Compression started');
+
         CommandHelper::executeShellCommand("tar --exclude=.gitignore --mode=0777 --transform \"s|web/resources||\" --transform \"s|" . substr($sql_path, 1) . "|database.sql|\" -zcvf $zip_path $sql_path $thumbnail_dir $screenshot_dir $featuredimage_dir $programs_dir $mediapackage_dir $template_dir",
-            array('timeout' => 7200), "Create tar.gz file", $output);
-        CommandHelper::executeShellCommand("chmod 777 ".$zip_path, array(), "Set permissions", $output);
+            array('timeout' => 7200));
+        $progress->advance();
+        $progress->setMessage('Compression finished. Setting permissions.');
+
+        CommandHelper::executeShellCommand("chmod 777 ".$zip_path, array());
+        $progress->advance();
+        $progress->setMessage('Permissions set.');
 
 
         unlink($sql_path);
-        $this->output->writeln('Finished! Backupfile created at '.$zip_path);
+        $progress->setMessage("Temp sql file deleted. Finished!\n Backupfile created at " . $zip_path . "\n");
+        $progress->finish();
+
     }
 }
