fc15bde547b8f231d32db7674355108562c12313
Kevin Haslinger <kevin.haslinger@student.tugraz.at>
2017-04-21 12:28:58 +0200

    make search from header work, add css to manipulate yeti style, update gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,34 +6,35 @@ var themes = ['pocketcode', 'pocketalice', 'pocketgalaxy'];
 
 var lessconfig = {};
 
-for (index = 0; index < themes.length; index++) {
-    var theme = themes[index];
-
-    var base_css_path = "web/css/" + theme + "/base.css";
-
-    var base_file_config = {};
-    base_file_config[base_css_path] = ["web/css/plugins/*", "web/css/themes/" + theme + "/main.less"];
-
-    lessconfig[theme] =
+for (index = 0; index < themes.length; index++)
+{
+  var theme = themes[index];
+  
+  var base_css_path = "web/css/" + theme + "/base.css";
+  
+  var base_file_config = {};
+  base_file_config[base_css_path] = ["web/css/plugins/*", "web/css/themes/" + theme + "/main.less"];
+  
+  lessconfig[theme] =
     {
-        options: {
-            compress: true,
-            yuicompress: true,
-            optimization: 2,
-            relativeUrls: true,
-            paths: ["web/css/base", "web/css/themes/" + theme]
-        },
-        files: [
-            base_file_config,
-            {
-                expand: true,
-                cwd: 'web/css/custom/',
-                src: ['**/*.less'],
-                dest: 'web/css/' + theme + '/',
-                ext: '.css',
-                extDot: 'first'
-            }
-        ]
+      options: {
+        compress    : true,
+        yuicompress : true,
+        optimization: 2,
+        relativeUrls: true,
+        paths       : ["web/css/base", "web/css/themes/" + theme]
+      },
+      files  : [
+        base_file_config,
+        {
+          expand: true,
+          cwd   : 'web/css/custom/',
+          src   : ['**/*.less'],
+          dest  : 'web/css/' + theme + '/',
+          ext   : '.css',
+          extDot: 'first'
+        }
+      ]
     };
 }
 
@@ -42,121 +43,133 @@ var admin_file_config = {};
 admin_file_config[admin_css_path] = ["web/css/plugins/*"];
 
 lessconfig['admin'] = {
-    options: {
-        compress: true,
-        yuicompress: true,
-        optimization: 2,
-        relativeUrls: true,
-    },
-    files: [
-        admin_file_config,
-        {
-            expand: true,
-            cwd: 'web/css/admin/',
-            src: ['**/*.less'],
-            dest: 'web/css/admin/',
-            ext: '.css',
-            extDot: 'first'
-        }
-    ]
+  options: {
+    compress    : true,
+    yuicompress : true,
+    optimization: 2,
+    relativeUrls: true,
+  },
+  files  : [
+    admin_file_config,
+    {
+      expand: true,
+      cwd   : 'web/css/admin/',
+      src   : ['**/*.less'],
+      dest  : 'web/css/admin/',
+      ext   : '.css',
+      extDot: 'first'
+    }
+  ]
 };
 
-module.exports = function (grunt) {
-    require('jit-grunt')(grunt);
-
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        concat: {
-            options: {
-                separator: ';',
-                banner: '/*\n  Generated File by Grunt\n  Sourcepath: web/js\n*/\n'
-            },
-            base: {
-                src: jsBaseSrc,
-                dest: 'web/compiled/js/<%= pkg.baseJSName %>.js'
-            },
-            login: {
-                src: jsLoginSrc,
-                dest: 'web/compiled/js/<%= pkg.loginJSName %>.js'
-            },
-            localPlugins: {
-                expand: true,
-                cwd: jsLocalPluginSrc,
-                src: '**/*.js',
-                dest: 'web/compiled/js/'
-            },
-            custom: {
-                expand: true,
-                cwd: jsCustomSrc,
-                src: '**/*.js',
-                dest: 'web/compiled/js/'
-            },
-            bootstrapcss: {
-                expand: true,
-                cwd: 'vendor/twbs/bootstrap/dist/css',
-                src: 'bootstrap.min.css',
-                dest: 'web/compiled/bootstrap/'
-            },
-            bootstrapjs: {
-                expand: true,
-                cwd: 'vendor/twbs/bootstrap/dist/js',
-                src: 'bootstrap.min.js',
-                dest: 'web/compiled/bootstrap/'
-            },
-            jquery: {
-                expand: true,
-                cwd: 'node_modules/jquery/dist',
-                src: 'jquery.min.js',
-                dest: 'web/compiled/bootstrap/'
-            },
-        },
-        uglify: {
-            options: {
-                mangle: false
-            },
-            compiledFiles: {
-                files: [{
-                    expand: true,
-                    cwd: 'web/compiled/js',
-                    src: '**/*.js',
-                    dest: 'web/compiled/min'
-                }]
-            }
-        },
-        cssmin: {
-            target: {
-                files: [{
-                    expand: true,
-                    cwd: 'web/css/bootstrap_yeti/',
-                    src: 'bootstrap_yeti.css',
-                    dest: 'web/compiled/bootstrap/',
-                    ext: '.min.css',
-                }]
-            }
-        },
-        less: lessconfig,
-        watch: {
-            options: {
-                nospawn: true
-            },
-            styles: {
-                files: ['web/css/**/*.less'],
-                tasks: ['less'],
-                options: {
-                    nospawn: true
-                }
-            },
-            scripts: {
-                files: ['web/js/**/*.js'],
-                tasks: ['concat','uglify'],
-                options: {
-                    nospawn: true
-                }
-            }
+module.exports = function(grunt)
+{
+  require('jit-grunt')(grunt);
+  
+  grunt.initConfig({
+    pkg   : grunt.file.readJSON('package.json'),
+    concat: {
+      options     : {
+        separator: ';',
+        banner   : '/*\n  Generated File by Grunt\n  Sourcepath: web/js\n*/\n'
+      },
+      base        : {
+        src : jsBaseSrc,
+        dest: 'web/compiled/js/<%= pkg.baseJSName %>.js'
+      },
+      login       : {
+        src : jsLoginSrc,
+        dest: 'web/compiled/js/<%= pkg.loginJSName %>.js'
+      },
+      localPlugins: {
+        expand: true,
+        cwd   : jsLocalPluginSrc,
+        src   : '**/*.js',
+        dest  : 'web/compiled/js/'
+      },
+      custom      : {
+        expand: true,
+        cwd   : jsCustomSrc,
+        src   : '**/*.js',
+        dest  : 'web/compiled/js/'
+      },
+      bootstrapcss: {
+        expand: true,
+        cwd   : 'vendor/twbs/bootstrap/dist/css',
+        src   : 'bootstrap.min.css',
+        dest  : 'web/compiled/bootstrap/'
+      },
+      bootstrapjs : {
+        expand: true,
+        cwd   : 'vendor/twbs/bootstrap/dist/js',
+        src   : 'bootstrap.min.js',
+        dest  : 'web/compiled/bootstrap/'
+      },
+      jquery      : {
+        expand: true,
+        cwd   : 'node_modules/jquery/dist',
+        src   : 'jquery.min.js',
+        dest  : 'web/compiled/bootstrap/'
+      },
+    },
+    cssmin: {
+      target: {
+        files: [
+          {
+            expand: true,
+            cwd   : 'web/css/bootstrap_yeti/',
+            src   : 'bootstrap_yeti.css',
+            dest  : 'web/compiled/bootstrap/',
+            ext   : '.min.css',
+          },
+          {
+            expand: true,
+            cwd   : 'web/css/bootstrap_yeti/',
+            src   : 'bootstrap_yeti_adjustments.css',
+            dest  : 'web/compiled/bootstrap/',
+            ext   : '.min.css',
+          }
+        ]
+      }
+    },
+    uglify: {
+      options      : {
+        mangle: false
+      },
+      compiledFiles: {
+        files: [
+          {
+            expand: true,
+            cwd   : 'web/compiled/js',
+            src   : '**/*.js',
+            dest  : 'web/compiled/min'
+          }
+        ]
+      }
+    },
+    less  : lessconfig,
+    watch : {
+      options: {
+        nospawn: true
+      },
+      styles : {
+        files  : ['web/css/**/*.less'],
+        tasks  : ['less'],
+        options: {
+          nospawn: true
         }
-    });
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.registerTask('default', ['concat', 'less', 'uglify', 'cssmin', 'watch']);
+      },
+      scripts: {
+        files  : ['web/js/**/*.js'],
+        tasks  : ['concat', 'uglify'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['concat', 'less', 'cssmin', 'uglify', 'watch']);
 };
--- web/compiled/js/script.js
+++ web/compiled/js/script.js
@@ -2,138 +2,169 @@
   Generated File by Grunt
   Sourcepath: web/js
 */
-var Main = function (search_url) {
-    var self = this;
-    self.search_url = search_url.replace(0, '');
-
-    $(window).ready(function () {
-        self.setClickListener();
-        self.setWindowResizeListener();
-
-        $("#feature-slider").owlCarousel({
-            responsive: true,
-            items: 1,
-            singleItem: true,
-            autoPlay: 5000
+var Main = function(search_url)
+{
+  var self = this;
+  self.search_url = search_url.replace(0, '');
+  
+  $(window).ready(function()
+  {
+    self.setClickListener();
+    self.setWindowResizeListener();
+    
+    $("#feature-slider").owlCarousel({
+      responsive: true,
+      items     : 1,
+      singleItem: true,
+      autoPlay  : 5000
+    });
+  });
+  $(document).ready(function()
+  {
+    //var s = 'script';
+    //var id = 'facebook-jssdk';
+    //var js, fjs = document.getElementsByTagName(s)[0];
+    //if (document.getElementById(id)) return;
+    //js = document.createElement(s);
+    //js.id = id;
+    //js.src = "//connect.facebook.net/de_DE/sdk.js#xfbml=1&version=v2.5";
+    //fjs.parentNode.insertBefore(js, fjs);
+    $.ajaxSetup({cache: true});
+    $.getScript('//connect.facebook.net/en_US/sdk.js', function()
+    {
+      var $appid = '';
+      var $ajaxGetFBAppId = Routing.generate(
+        'catrobat_oauth_login_get_facebook_appid', {flavor: 'pocketcode'}
+      );
+      $.get($ajaxGetFBAppId,
+        function(data)
+        {
+          console.log(data);
+          $appid = data['fb_appid'];
+          FB.init({
+            appId  : $appid,
+            xfbml  : true,
+            status : true,
+            cookie : true,  //allow the server to access the session
+            version: 'v2.6'
+          });
         });
     });
-    $(document).ready(function () {
-        //var s = 'script';
-        //var id = 'facebook-jssdk';
-        //var js, fjs = document.getElementsByTagName(s)[0];
-        //if (document.getElementById(id)) return;
-        //js = document.createElement(s);
-        //js.id = id;
-        //js.src = "//connect.facebook.net/de_DE/sdk.js#xfbml=1&version=v2.5";
-        //fjs.parentNode.insertBefore(js, fjs);
-        $.ajaxSetup({ cache: true });
-        $.getScript('//connect.facebook.net/en_US/sdk.js', function () {
-            var $appid = '';
-            var $ajaxGetFBAppId = Routing.generate(
-                'catrobat_oauth_login_get_facebook_appid', {flavor: 'pocketcode'}
-            );
-            $.get($ajaxGetFBAppId,
-                function (data) {
-                    console.log(data);
-                    $appid = data['fb_appid'];
-                    FB.init({
-                        appId: $appid,
-                        xfbml: true,
-                        status: true,
-                        cookie: true,  //allow the server to access the session
-                        version: 'v2.6'
-                    });
-                });
-        });
-
-        //Google+ JS API:
-        var po = document.createElement('script');
-        po.type = 'text/javascript';
-        po.async = true;
-        po.src = 'https://apis.google.com/js/client:plusone.js';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(po, s);
-    });
-
-  self.setClickListener = function() {
+    
+    //Google+ JS API:
+    var po = document.createElement('script');
+    po.type = 'text/javascript';
+    po.async = true;
+    po.src = 'https://apis.google.com/js/client:plusone.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(po, s);
+  });
+  
+  self.setClickListener = function()
+  {
     var nav = $('nav');
     var navDropdown = $('#nav-dropdown');
-
+    
     // toggle footer view
-    $('#footer-more-less').click(function () {
+    $('#footer-more-less').click(function()
+    {
       $('body').toggleClass('footer-expand');
       $(window).scrollTop($(document).height());
     });
-
+    
     // toggle searchbar
-    $('#menu-mobile').find('.btn-search').click(function() {
+    $('#menu-mobile').find('.btn-search').click(function()
+    {
       nav.toggleClass('searchbar-visible');
       nav.find('input').focus();
     });
-
+    
     // toggle navigation dropdown (when logged in)
-    $('.show-nav-dropdown').click(function() {
+    $('.show-nav-dropdown').click(function()
+    {
       var newPosition = nav.position().left + nav.outerWidth() - navDropdown.width();
       navDropdown.css('left', newPosition).toggle();
     });
-
-    $('#copy-link').click(function() {
+    
+    $('#copy-link').click(function()
+    {
       $(this).find('tr').first().hide();
       $(this).find('tr').last().show();
       $('#url-link').focus().select();
     });
-
+    
     self.setSearchBtnListener();
     self.setLanguageSwitchListener();
   };
-
-  self.setWindowResizeListener = function() {
-    $(window).resize(function() {
+  
+  self.setWindowResizeListener = function()
+  {
+    $(window).resize(function()
+    {
       $('#nav-dropdown').hide();
     });
   };
-
-  self.setSearchBtnListener = function() {
+  
+  self.setSearchBtnListener = function()
+  {
     // search enter pressed
-    $('.input-search').keypress(function(event) {
-      if(event.which == 13) self.searchPrograms($(this).val());
+    $('.input-search').keypress(function(event)
+    {
+      if (event.which == 13)
+      {
+        self.searchPrograms($(this).val());
+      }
     });
-
+    
     // search button clicked (header)
-    $('#menu').find('.img-magnifying-glass').click(function() {
-      self.searchPrograms($(this).parent().parent().find('input').val());
+    $('.btn-search').click(function()
+    {
+      self.searchPrograms($(this).prev().find('input').val());
     });
-
+    
     // search button clicked (footer)
-    $('#footer-menu-desktop').find('.img-magnifying-glass').click(function() {
-      self.searchPrograms($(this).parent().parent().find('input').val());
+    // TODO: when applying bootstrap to the footer this has to be changed to make it work
+    $('#footer-menu-desktop').find('.img-magnifying-glass').click(function()
+    {
+      self.searchPrograms($(this).prev().find('input').val());
     });
   };
-
-  self.searchPrograms = function(string) {
+  
+  self.searchPrograms = function(string)
+  {
     window.location.href = self.search_url + encodeURIComponent(string.trim());
   };
-
-  self.setLanguageSwitchListener = function() {
+  
+  self.setLanguageSwitchListener = function()
+  {
     var select = $('#switch-language');
-    select.change(function() {
-      document.cookie = 'hl=' + $(this).val() +  "; path=/";
+    select.change(function()
+    {
+      document.cookie = 'hl=' + $(this).val() + "; path=/";
       location.reload();
     });
   };
-
-  self.getCookie = function(cname) {
+  
+  self.getCookie = function(cname)
+  {
     var name = cname + "=";
     var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
+    for (var i = 0; i < ca.length; i++)
+    {
       var c = ca[i];
-      while (c.charAt(0)==' ') c = c.substring(1);
-      if (c.indexOf(name) != -1) return c.substring(name.length,c.length);
+      while (c.charAt(0) == ' ')
+      {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) != -1)
+      {
+        return c.substring(name.length, c.length);
+      }
     }
     return "";
   };
-
-
+  
+  
 };
 ;var ProgramLoader = function (container, url, column_max, recommended_by_program_id, recommended_by_page_id) {
   var self = this;
