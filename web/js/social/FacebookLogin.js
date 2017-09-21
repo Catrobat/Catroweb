465bd6fa16c3ebead7b78f6f5e015c383eb7811d
Eric Gergely <gergely@student.tugraz.at>
2017-09-21 16:47:21 +0200

    Terms of Condition

--- web/js/social/FacebookLogin.js
+++ web/js/social/FacebookLogin.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    var agree_facebook = false;
+    var facebook_login_triggered = false;
     $.ajaxSetup({ cache: true });
     $.getScript('//connect.facebook.net/en_US/sdk.js', function () {
         var $appid = '';
@@ -22,87 +24,98 @@ $(document).ready(function () {
         FacebookLogout();
     });
     $("#_submit_oauth").attr("disabled", true);
-});
 
-function triggerFacebookLogin() {
-    FB.login(function(response) {
-        if (response.authResponse) {
-            console.log('Facebook Login successful');
-            checkLoginState();
-        } else {
-            console.log('User cancelled login or did not fully authorize.');
-        }
-    }, {
-        //scope: 'public_profile,email,user_about_me,manage_pages,publish_pages', manage_pages,publish_pages are necessary when requesting new token for fb post
-        scope: 'public_profile,email',
-        return_scopes: true,
-        auth_type: $('#facebook_auth_type').val() //set to 'reauthenticate' to force re-authentication of the user
+    $('#agreeButton').click(function () {
+        agree_facebook = true;
+        if(facebook_login_triggered)
+            triggerFacebookLogin();
     });
-}
 
-function checkLoginState() {
-    console.log("checkLoginState");
-    FB.getLoginStatus(function (response) {
-        statusChangeCallback(response);
-    });
-}
 
-function statusChangeCallback(response) {
-    if (response.status === 'connected') {
-        $("#access_token_oauth").val(response['authResponse'].accessToken);
-        getFacebookUserInfo();
-    } else if (response.status === 'not_authorized') {
-        // The person is logged into Facebook, but not your app.
-        document.getElementById('status').innerHTML = 'Please sign in to Pocket Code';
-    } else {
-        // The person is not logged into Facebook, so we're not sure if
-        // they are logged into this app or not.
-        document.getElementById('status').innerHTML = 'Please log into Facebook';
+    function triggerFacebookLogin() {
+        facebook_login_triggered = true;
+        if(!agree_facebook)
+            return false;
+
+        facebook_login_triggered = false;
+        FB.login(function(response) {
+            if (response.authResponse) {
+                console.log('Facebook Login successful');
+                checkLoginState();
+            } else {
+                console.log('User cancelled login or did not fully authorize.');
+            }
+        }, {
+            //scope: 'public_profile,email,user_about_me,manage_pages,publish_pages', manage_pages,publish_pages are necessary when requesting new token for fb post
+            scope: 'public_profile,email',
+            return_scopes: true,
+            auth_type: $('#facebook_auth_type').val() //set to 'reauthenticate' to force re-authentication of the user
+        });
     }
-}
 
-function getFacebookUserInfo() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function (response) {
-        console.log('Successful login for: ' + response.name);
-        console.log("First name:" + response.first_name);
-        console.log("Last name:" + response.last_name);
-        console.log("Name:" + response.name);
-        console.log("Response ID:" + response.id);
-        console.log("Country:" + response.locale);
+    function checkLoginState() {
+        console.log("checkLoginState");
+        FB.getLoginStatus(function (response) {
+            statusChangeCallback(response);
+        });
+    }
 
-        $("#id_oauth").val(response.id);
-        $("#email_oauth").val(response.email);
-        $("#locale_oauth").val(response.locale);
+    function statusChangeCallback(response) {
+        if (response.status === 'connected') {
+            $("#access_token_oauth").val(response['authResponse'].accessToken);
+            getFacebookUserInfo();
+        } else if (response.status === 'not_authorized') {
+            // The person is logged into Facebook, but not your app.
+            document.getElementById('status').innerHTML = 'Please sign in to Pocket Code';
+        } else {
+            // The person is not logged into Facebook, so we're not sure if
+            // they are logged into this app or not.
+            document.getElementById('status').innerHTML = 'Please log into Facebook';
+        }
+    }
 
-        handleFacebookUserInfoResponseTrigger();
-    });
-}
+    function getFacebookUserInfo() {
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me', function (response) {
+            console.log('Successful login for: ' + response.name);
+            console.log("First name:" + response.first_name);
+            console.log("Last name:" + response.last_name);
+            console.log("Name:" + response.name);
+            console.log("Response ID:" + response.id);
+            console.log("Country:" + response.locale);
 
-var handleFacebookUserInfoResponseTrigger = function handleFacebookUserInfoResponse() {
+            $("#id_oauth").val(response.id);
+            $("#email_oauth").val(response.email);
+            $("#locale_oauth").val(response.locale);
 
-    $id = $("#id_oauth").val();
-    $email = $("#email_oauth").val();
-    $locale = $("#locale_oauth").val();
+            handleFacebookUserInfoResponseTrigger();
+        });
+    }
 
-    var $ajaxUrlCheckServerTokenAvailable = Routing.generate(
-        'catrobat_oauth_login_facebook_servertoken_available', {flavor: 'pocketcode'}
-    );
+    var handleFacebookUserInfoResponseTrigger = function handleFacebookUserInfoResponse() {
 
-    $.post($ajaxUrlCheckServerTokenAvailable,
-        {
-            id: $id
-        },
-        function (data, status) {
-            console.log(data);
-            console.log(status);
-            var $server_token_available = data['token_available'];
-            if (!$server_token_available) {
+        $id = $("#id_oauth").val();
+        $email = $("#email_oauth").val();
+        $locale = $("#locale_oauth").val();
 
-                var $ajaxUrlCheckEmailAvailable = Routing.generate(
+        var $ajaxUrlCheckServerTokenAvailable = Routing.generate(
+          'catrobat_oauth_login_facebook_servertoken_available', {flavor: 'pocketcode'}
+        );
+
+        $.post($ajaxUrlCheckServerTokenAvailable,
+          {
+              id: $id
+          },
+          function (data, status) {
+              console.log(data);
+              console.log(status);
+              var $server_token_available = data['token_available'];
+              if (!$server_token_available) {
+
+                  var $ajaxUrlCheckEmailAvailable = Routing.generate(
                     'catrobat_oauth_login_email_available', {flavor: 'pocketcode'}
-                );
-                $.post($ajaxUrlCheckEmailAvailable,
+                  );
+                  $.post($ajaxUrlCheckEmailAvailable,
                     {
                         email: $email
                     },
@@ -116,60 +129,60 @@ var handleFacebookUserInfoResponseTrigger = function handleFacebookUserInfoRespo
                             sendTokenToServer($("#access_token_oauth").val(), $id, data['username'], $email, $locale)
                         }
                     });
-            } else {
-                FacebookLogin(data['email'], data['username'], $id, $locale);
-            }
-        });
-};
+              } else {
+                  FacebookLogin(data['email'], data['username'], $id, $locale);
+              }
+          });
+    };
 
-function getDesiredUsernameFB() {
-    $("#fb_google").val('fb');
-    openDialog();
-}
+    function getDesiredUsernameFB() {
+        $("#fb_google").val('fb');
+        openDialog();
+    }
 
-function sendTokenToServer($token, $facebook_id, $username, $email, $locale) {
+    function sendTokenToServer($token, $facebook_id, $username, $email, $locale) {
 
-    var $state = $('#csrf_token').val();
+        var $state = $('#csrf_token').val();
 
-    var $ajaxUrl = Routing.generate(
-        'catrobat_oauth_login_facebook_token', {flavor: 'pocketcode'}
-    );
+        var $ajaxUrl = Routing.generate(
+          'catrobat_oauth_login_facebook_token', {flavor: 'pocketcode'}
+        );
 
-    $.post($ajaxUrl,
-        {
-            client_token: $token,
-            id: $facebook_id,
-            state: $state,
-            username: $username,
-            email: $email,
-            locale: $locale
-        },
-        function (data, status) {
-            console.log(data);
-            console.log(status);
-            FacebookLogin($email, $username, $facebook_id, $locale);
-        });
-}
+        $.post($ajaxUrl,
+          {
+              client_token: $token,
+              id: $facebook_id,
+              state: $state,
+              username: $username,
+              email: $email,
+              locale: $locale
+          },
+          function (data, status) {
+              console.log(data);
+              console.log(status);
+              FacebookLogin($email, $username, $facebook_id, $locale);
+          });
+    }
 
-function FacebookLogin($email, $username, $id, $locale) {
+    function FacebookLogin($email, $username, $id, $locale) {
 
-    var $ajaxUrl = Routing.generate(
-        'catrobat_oauth_login_facebook', {flavor: 'pocketcode'}
-    );
+        var $ajaxUrl = Routing.generate(
+          'catrobat_oauth_login_facebook', {flavor: 'pocketcode'}
+        );
 
-    $.post($ajaxUrl,
-        {
-            username: $username,
-            id: $id,
-            email: $email,
-            locale: $locale
-        },
-        function (data, status) {
-            var $ajaxLoginRedirectUrl = Routing.generate(
+        $.post($ajaxUrl,
+          {
+              username: $username,
+              id: $id,
+              email: $email,
+              locale: $locale
+          },
+          function (data, status) {
+              var $ajaxLoginRedirectUrl = Routing.generate(
                 'catrobat_oauth_login_redirect', {flavor: 'pocketcode'}
-            );
+              );
 
-            $.post($ajaxLoginRedirectUrl,
+              $.post($ajaxLoginRedirectUrl,
                 {
                     fb_id: $id
                 }, function (data, status) {
@@ -178,16 +191,20 @@ function FacebookLogin($email, $username, $id, $locale) {
                     $(location).attr('href', $url);
                 });
 
-        });
-}
+          });
+    }
 
-function FacebookLogout() {
-    FB.getLoginStatus(function (response) {
-        if (response.status === 'connected') {
-            FB.logout(function (logout_response) {
-                console.log('User logged out of Facebook with response:');
-                console.log(logout_response);
-            });
-        }
-    }, true);
-}
+    function FacebookLogout() {
+        FB.getLoginStatus(function (response) {
+            if (response.status === 'connected') {
+                FB.logout(function (logout_response) {
+                    console.log('User logged out of Facebook with response:');
+                    console.log(logout_response);
+                });
+            }
+        }, true);
+    }
+
+
+});
+
--- web/js/social/GoogleLogin.js
+++ web/js/social/GoogleLogin.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    var agree_oauth = false;
+    var google_triggered = false;
     var po = document.createElement('script');
     po.type = 'text/javascript';
     po.async = true;
@@ -9,90 +11,100 @@ $(document).ready(function () {
         GoogleLogout();
     });
     $("#_submit_oauth").attr("disabled", true);
-});
 
-function triggerGoogleLogin(){
-    var $appid = '';
-    var $ajaxGetGoogleAppId = Routing.generate(
-        'catrobat_oauth_login_get_google_appid', {flavor: 'pocketcode'}
-    );
-    $.get($ajaxGetGoogleAppId,
-        function (data) {
-            console.log(data);
-            var $appid = data['gplus_appid'];
-            gapi.signin.render('googleLoginButton', {
-                'callback': 'signinCallback',
-                'approvalprompt': $('#gplus_approval_prompt').val(), //'force' prevents auto g+-signin
-                'clientid': $appid,
-                'cookiepolicy': 'single_host_origin',
-                'redirecturi': 'postmessage',
-                'accesstype': 'offline',
-                'scope': 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email'
-            })
-        });
-}
+    function triggerGoogleLogin(){
+        var $appid = '';
+        google_triggered = true;
+        if(!agree_oauth)
+            return false;
 
-function signinCallback(authResult) {
-    if (authResult['code']) {
-        $("#access_token_oauth").val(authResult['code']);
-        console.log('auth: ' + authResult['code']);
-        getGoogleUserInfo(authResult);
-    } else if (authResult['error']) {
-        if(authResult['error'] == 'access_denied') {
-            console.log('User denied access to the app');
-        } else if(authResult['error'] == 'immediate_failed') {
-            console.log('Automatic sign-in of user failed');
-        } else {
-            console.log('error:' + authResult['error']);
+        var $ajaxGetGoogleAppId = Routing.generate(
+          'catrobat_oauth_login_get_google_appid', {flavor: 'pocketcode'}
+        );
+        google_triggered = false;
+        $.get($ajaxGetGoogleAppId,
+          function (data) {
+              console.log(data);
+              var $appid = data['gplus_appid'];
+              gapi.signin.render('googleLoginButton', {
+                  'callback': 'signinCallback',
+                  'approvalprompt': $('#gplus_approval_prompt').val(), //'force' prevents auto g+-signin
+                  'clientid': $appid,
+                  'cookiepolicy': 'single_host_origin',
+                  'redirecturi': 'postmessage',
+                  'accesstype': 'offline',
+                  'scope': 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email'
+              })
+          });
+    }
+
+    function signinCallback(authResult) {
+        if (authResult['code']) {
+            $("#access_token_oauth").val(authResult['code']);
+            console.log('auth: ' + authResult['code']);
+            getGoogleUserInfo(authResult);
+        } else if (authResult['error']) {
+            if(authResult['error'] == 'access_denied') {
+                console.log('User denied access to the app');
+            } else if(authResult['error'] == 'immediate_failed') {
+                console.log('Automatic sign-in of user failed');
+            } else {
+                console.log('error:' + authResult['error']);
+            }
         }
     }
-}
 
-function getGoogleUserInfo(authResult) {
-    gapi.client.load('oauth2', 'v2', function () {
-        var request = gapi.client.oauth2.userinfo.get();
-        request.execute(getUserInfoCallback);
+    $('#agreeButton').click(function () {
+        agree_oauth = true;
+        if(google_triggered)
+            triggerGoogleLogin();
     });
 
-    function getUserInfoCallback(obj) {
-        if (obj['email']) {
-            $("#email_oauth").val(obj['email']);
+    function getGoogleUserInfo(authResult) {
+        gapi.client.load('oauth2', 'v2', function () {
+            var request = gapi.client.oauth2.userinfo.get();
+            request.execute(getUserInfoCallback);
+        });
+
+        function getUserInfoCallback(obj) {
+            if (obj['email']) {
+                $("#email_oauth").val(obj['email']);
+            }
+            if (obj['id']) {
+                $("#id_oauth").val(obj['id']);
+            }
+            if (obj['locale']) {
+                $("#locale_oauth").val(obj['locale']);
+            }
+            checkGoogleCallbackDataWithServer();
         }
-        if (obj['id']) {
-            $("#id_oauth").val(obj['id']);
-        }
-        if (obj['locale']) {
-            $("#locale_oauth").val(obj['locale']);
-        }
-        checkGoogleCallbackDataWithServer();
     }
-}
 
-function checkGoogleCallbackDataWithServer() {
+    function checkGoogleCallbackDataWithServer() {
 
-    $id = $("#id_oauth").val();
-    $email = $("#email_oauth").val();
-    $locale = $("#locale_oauth").val();
+        $id = $("#id_oauth").val();
+        $email = $("#email_oauth").val();
+        $locale = $("#locale_oauth").val();
 
-    var $ajaxUrlCheckServerTokenAvailable = Routing.generate(
-        'catrobat_oauth_login_google_servertoken_available', {flavor: 'pocketcode'}
-    );
-    $.post($ajaxUrlCheckServerTokenAvailable,
-        {
-            id: $id
-        },
-        function (data) {
-            console.log(data);
-            var $server_token_available = data['token_available'];
-            if (!$server_token_available) {
-                $("#id_oauth").val($id);
-                $("#email_oauth").val($email);
-                $("#locale_oauth").val($locale);
+        var $ajaxUrlCheckServerTokenAvailable = Routing.generate(
+          'catrobat_oauth_login_google_servertoken_available', {flavor: 'pocketcode'}
+        );
+        $.post($ajaxUrlCheckServerTokenAvailable,
+          {
+              id: $id
+          },
+          function (data) {
+              console.log(data);
+              var $server_token_available = data['token_available'];
+              if (!$server_token_available) {
+                  $("#id_oauth").val($id);
+                  $("#email_oauth").val($email);
+                  $("#locale_oauth").val($locale);
 
-                var $ajaxUrlCheckEmailAvailable = Routing.generate(
+                  var $ajaxUrlCheckEmailAvailable = Routing.generate(
                     'catrobat_oauth_login_email_available', {flavor: 'pocketcode'}
-                );
-                $.post($ajaxUrlCheckEmailAvailable,
+                  );
+                  $.post($ajaxUrlCheckEmailAvailable,
                     {
                         email: $email
                     },
@@ -106,59 +118,59 @@ function checkGoogleCallbackDataWithServer() {
                             sendCodeToServer($("#access_token_oauth").val(), $id, data['username'], $email, $locale);
                         }
                     });
-            } else {
-                GoogleLogin(data['email'], data['username'], $id, $locale);
-            }
-        });
-}
+              } else {
+                  GoogleLogin(data['email'], data['username'], $id, $locale);
+              }
+          });
+    }
 
-function getDesiredUsernameGoogle() {
-    $("#fb_google").val('g+');
-    openDialog();
-}
+    function getDesiredUsernameGoogle() {
+        $("#fb_google").val('g+');
+        openDialog();
+    }
 
-function sendCodeToServer($code, $gplus_id, $username, $email, $locale) {
+    function sendCodeToServer($code, $gplus_id, $username, $email, $locale) {
 
-    var $state = $('#csrf_token').val();
-    var $ajaxUrl = Routing.generate(
-        'catrobat_oauth_login_google_code', {flavor: 'pocketcode'}
-    );
+        var $state = $('#csrf_token').val();
+        var $ajaxUrl = Routing.generate(
+          'catrobat_oauth_login_google_code', {flavor: 'pocketcode'}
+        );
 
-    $.post($ajaxUrl,
-        {
-            code: $code,
-            id: $gplus_id,
-            state: $state,
-            username: $username,
-            email: $email,
-            locale: $locale
-        },
-        function (data, status) {
-            console.log(status);
-            console.log(data);
-            GoogleLogin($email, $username, $gplus_id, $locale);
-        });
-}
+        $.post($ajaxUrl,
+          {
+              code: $code,
+              id: $gplus_id,
+              state: $state,
+              username: $username,
+              email: $email,
+              locale: $locale
+          },
+          function (data, status) {
+              console.log(status);
+              console.log(data);
+              GoogleLogin($email, $username, $gplus_id, $locale);
+          });
+    }
 
-function GoogleLogin($email, $username, $id, $locale) {
+    function GoogleLogin($email, $username, $id, $locale) {
 
-    var $ajaxUrl = Routing.generate(
-        'catrobat_oauth_login_google', {flavor: 'pocketcode'}
-    );
+        var $ajaxUrl = Routing.generate(
+          'catrobat_oauth_login_google', {flavor: 'pocketcode'}
+        );
 
-    $.post($ajaxUrl,
-        {
-            username: $username,
-            id: $id,
-            email: $email,
-            locale: $locale
-        },
-        function (data, status) {
-            var $ajaxLoginRedirectUrl = Routing.generate(
+        $.post($ajaxUrl,
+          {
+              username: $username,
+              id: $id,
+              email: $email,
+              locale: $locale
+          },
+          function (data, status) {
+              var $ajaxLoginRedirectUrl = Routing.generate(
                 'catrobat_oauth_login_redirect', {flavor: 'pocketcode'}
-            );
+              );
 
-            $.post($ajaxLoginRedirectUrl,
+              $.post($ajaxLoginRedirectUrl,
                 {
                     gplus_id: $id
                 }, function (data, status) {
@@ -166,24 +178,26 @@ function GoogleLogin($email, $username, $id, $locale) {
                     $url = data['url'];
                     $(location).attr('href', $url);
                 });
-        });
-}
+          });
+    }
 
-function GoogleLogout() {
-    var $appid = '';
-    var $ajaxGetGoogleAppId = Routing.generate(
-        'catrobat_oauth_login_get_google_appid', {flavor: 'pocketcode'}
-    );
-    $.get($ajaxGetGoogleAppId,
-        function (data) {
-            var sessionParams = {
-                'client_id': data['gplus_appid'],
-                'session_state': null
-            };
-            gapi.auth.checkSessionState(sessionParams, function (connected) {
-                if (connected) {
-                    gapi.auth.signOut()
-                }
-            });
-        });
-}
+    function GoogleLogout() {
+        var $appid = '';
+        var $ajaxGetGoogleAppId = Routing.generate(
+          'catrobat_oauth_login_get_google_appid', {flavor: 'pocketcode'}
+        );
+        $.get($ajaxGetGoogleAppId,
+          function (data) {
+              var sessionParams = {
+                  'client_id': data['gplus_appid'],
+                  'session_state': null
+              };
+              gapi.auth.checkSessionState(sessionParams, function (connected) {
+                  if (connected) {
+                      gapi.auth.signOut()
+                  }
+              });
+          });
+    }
+});
+
