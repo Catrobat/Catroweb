1e61a3bd0d8db7d0ad27247716527e55d9f2ef17
Daniel Metzner <metzner@student.tugraz.at>
2018-08-17 13:42:43 +0200

    usermanager uses now passwordUpdater and canonicalFieldsUpdater

--- spec/Catrobat/AppBundle/Entity/UserManagerSpec.php
+++ spec/Catrobat/AppBundle/Entity/UserManagerSpec.php
@@ -6,23 +6,21 @@ use Catrobat\AppBundle\Entity\User;
 use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\Mapping\ClassMetadata;
-use FOS\UserBundle\Util\CanonicalizerInterface;
+use FOS\UserBundle\Util\CanonicalFieldsUpdater;
+use FOS\UserBundle\Util\PasswordUpdaterInterface;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
-use Symfony\Component\Security\Core\Encoder\BasePasswordEncoder;
-use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
+
 
 class UserManagerSpec extends ObjectBehavior
 {
 
-    public function let(EncoderFactoryInterface $encoder_factory, CanonicalizerInterface $username_canonicalizer, CanonicalizerInterface $email_canonicalizer, ObjectManager $object_manager, User $user, ClassMetadata $meta, EntityRepository $repository, BasePasswordEncoder $password_encoder)
+    public function let(PasswordUpdaterInterface $passwordUpdater, CanonicalFieldsUpdater $canonicalFieldsUpdater,
+                        ObjectManager $object_manager, User $user, ClassMetadata $meta, EntityRepository $repository)
     {
-        $password_encoder->isPasswordValid(Argument::any(), '123', Argument::any())->willReturn(true);
-        $password_encoder->isPasswordValid(Argument::any(), 'abc', Argument::any())->willReturn(false);
-        $encoder_factory->getEncoder(Argument::any())->willReturn($password_encoder);
         $object_manager->getClassMetadata(Argument::any())->willReturn($meta);
         $object_manager->getRepository(Argument::any())->willReturn($repository);
-        $this->beConstructedWith($encoder_factory, $username_canonicalizer, $email_canonicalizer, $object_manager, $user);
+        $this->beConstructedWith($passwordUpdater, $canonicalFieldsUpdater, $object_manager, $user);
     }
 
     public function it_is_initializable()
@@ -30,9 +28,9 @@ class UserManagerSpec extends ObjectBehavior
         $this->shouldHaveType('Catrobat\AppBundle\Entity\UserManager');
     }
 
-    public function it_checks_if_password_is_valid(User $user, ObjectManager $object_manager, EntityRepository $repository)
-    {
-        $this->isPasswordValid($user, '123')->shouldBe(true);
-        $this->isPasswordValid($user, 'abc')->shouldBe(false);
-    }
+//    public function it_checks_if_password_is_valid(User $user, ObjectManager $object_manager, EntityRepository $repository)
+//    {
+//        $this->isPasswordValid($user, '123')->shouldBe(true);
+//        $this->isPasswordValid($user, 'abc')->shouldBe(false);
+//    }
 }
--- src/Catrobat/AppBundle/Entity/UserManager.php
+++ src/Catrobat/AppBundle/Entity/UserManager.php
@@ -9,13 +9,10 @@ use FOS\UserBundle\Util\PasswordUpdaterInterface;
 
 class UserManager extends \Sonata\UserBundle\Entity\UserManager
 {
-    public function __construct(PasswordUpdaterInterface $passwordUpdater, CanonicalFieldsUpdater $canonicalFieldsUpdater, ObjectManager $om, $class)
+    public function __construct(PasswordUpdaterInterface $passwordUpdater,
+                                CanonicalFieldsUpdater $canonicalFieldsUpdater,
+                                ObjectManager $om, $class)
     {
         parent::__construct($passwordUpdater, $canonicalFieldsUpdater, $om, $class);
-    }
-
-    public function isPasswordValid(UserInterface $user, $raw_password)
-    {
-        return $this->getEncoder($user)->isPasswordValid($user->getPassword(), $raw_password, $user->getSalt());
     }
 }
