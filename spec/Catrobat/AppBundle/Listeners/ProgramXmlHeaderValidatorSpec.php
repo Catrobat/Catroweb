08a8a99f19418db785e4b42e3032d4da7d88e920
Fabian <fabian.schnider@student.tugraz.at>
2018-08-17 12:46:00 +0200

    Fixed PHPSpec deprecation of annotations third.

--- spec/Catrobat/AppBundle/Listeners/ProgramXmlHeaderValidatorSpec.php
+++ spec/Catrobat/AppBundle/Listeners/ProgramXmlHeaderValidatorSpec.php
@@ -2,6 +2,7 @@
 
 namespace spec\Catrobat\AppBundle\Listeners;
 
+use Catrobat\AppBundle\Services\ExtractedCatrobatFile;
 use PhpSpec\ObjectBehavior;
 
 class ProgramXmlHeaderValidatorSpec extends ObjectBehavior
@@ -11,20 +12,14 @@ class ProgramXmlHeaderValidatorSpec extends ObjectBehavior
         $this->shouldHaveType('Catrobat\AppBundle\Listeners\ProgramXmlHeaderValidator');
     }
 
-  /**
-   * @param \Catrobat\AppBundle\Services\ExtractedCatrobatFile $file
-   */
-  public function it_checks_if_the_program_xml_header_is_valid($file)
+  public function it_checks_if_the_program_xml_header_is_valid(ExtractedCatrobatFile $file)
   {
       $xml = simplexml_load_file(__SPEC_GENERATED_FIXTURES_DIR__.'/base/code.xml');
       $file->getProgramXmlProperties()->willReturn($xml);
       $this->shouldNotThrow('Catrobat\AppBundle\Exceptions\Upload\InvalidXmlException')->duringValidate($file);
   }
 
-  /**
-   * @param \Catrobat\AppBundle\Services\ExtractedCatrobatFile $file
-   */
-  public function it_throws_an_exception_if_header_is_missing($file)
+  public function it_throws_an_exception_if_header_is_missing(ExtractedCatrobatFile $file)
   {
       $xml = simplexml_load_file(__SPEC_GENERATED_FIXTURES_DIR__.'/base/code.xml');
       unset($xml->header);
@@ -32,10 +27,7 @@ class ProgramXmlHeaderValidatorSpec extends ObjectBehavior
       $this->shouldThrow('Catrobat\AppBundle\Exceptions\Upload\InvalidXmlException')->duringValidate($file);
   }
 
-  /**
-   * @param \Catrobat\AppBundle\Services\ExtractedCatrobatFile $file
-   */
-  public function it_throws_an_exception_if_header_information_is_missing($file)
+  public function it_throws_an_exception_if_header_information_is_missing(ExtractedCatrobatFile $file)
   {
       $xml = simplexml_load_file(__SPEC_GENERATED_FIXTURES_DIR__.'/base/code.xml');
       unset($xml->header->applicationName);
@@ -43,10 +35,7 @@ class ProgramXmlHeaderValidatorSpec extends ObjectBehavior
       $this->shouldThrow('Catrobat\AppBundle\Exceptions\Upload\InvalidXmlException')->duringValidate($file);
   }
 
-  /**
-   * @param \Catrobat\AppBundle\Services\ExtractedCatrobatFile $file
-   */
-  public function it_checks_if_program_name_is_set($file)
+  public function it_checks_if_program_name_is_set(ExtractedCatrobatFile $file)
   {
       $xml = simplexml_load_file(__SPEC_GENERATED_FIXTURES_DIR__.'/base/code.xml');
       unset($xml->header->programName);
@@ -54,10 +43,7 @@ class ProgramXmlHeaderValidatorSpec extends ObjectBehavior
       $this->shouldThrow('Catrobat\AppBundle\Exceptions\Upload\InvalidXmlException')->duringValidate($file);
   }
 
-  /**
-   * @param \Catrobat\AppBundle\Services\ExtractedCatrobatFile $file
-   */
-  public function it_checks_if_description_is_set($file)
+  public function it_checks_if_description_is_set(ExtractedCatrobatFile $file)
   {
       $xml = simplexml_load_file(__SPEC_GENERATED_FIXTURES_DIR__.'/base/code.xml');
       unset($xml->header->description);
