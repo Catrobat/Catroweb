486f9bc4622369139201934e322f2430ab9bf46b
Fabian <fabian.schnider@student.tugraz.at>
2018-08-17 11:16:22 +0200

    Fixed PHPSpec setShared to define and added PHPSpec extension and packages

--- src/PhpSpec/CatrobatExtension/Extension.php
+++ src/PhpSpec/CatrobatExtension/Extension.php
@@ -9,10 +9,9 @@ class Extension implements \PhpSpec\Extension
     public function load(ServiceContainer $container, array $params = [])
     {
       $container->define('event_dispatcher.listeners.catrobat', function ($container) {
-      $listener = new CatrobatListener();
-      $listener->setIo($container->get('console.io'));
-
-      return $listener;
-    });
+          $listener = new CatrobatListener();
+          $listener->setIo($container->get('console.io'));
+          return $listener;
+      }, ['event_dispatcher.listeners']);
     }
 }
