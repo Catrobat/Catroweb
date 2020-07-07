3b802270c9aae12dcde0d00d1e2ccb14bd1d2c3b
Simon Posch <simon.posch@student.tugraz.at>
2020-07-07 15:37:30 +0200

    Rebase, disabled cache:clear for testing

--- docker/app/prepare-test-env.sh
+++ docker/app/prepare-test-env.sh
@@ -4,7 +4,7 @@ rm -rf var/{cache,log}/*
 
 sh docker/app/set-permissions.sh
 
-bin/console cache:clear -e test
+# bin/console cache:clear -e test
 bin/console assets:install --symlink public
 
 echo "Test environment prepared"
