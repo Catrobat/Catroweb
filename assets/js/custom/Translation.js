d7033a61c82101a980eb1a7b8056944e87643ac1
danielzhang130 <danielzhang130@gmail.com>
2020-11-24 14:09:12 -0500

    npm run fix

--- assets/js/custom/Translation.js
+++ assets/js/custom/Translation.js
@@ -1,16 +1,16 @@
 // eslint-disable-next-line no-unused-vars
-function Translation (textElements, srcLang, targetLang) {
-  let text = '';
+function Translation (textElements, srcLang) {
+  let text = ''
   if (Array.isArray(textElements)) {
-    let array = [];
+    const array = []
     for (let i = 0; i < textElements.length; i++) {
-      array.push(textElements[i].innerText);
+      array.push(textElements[i].innerText)
     }
 
-    text = array.join('\n');
+    text = array.join('\n')
   } else {
-    text = textElements.innerText;
+    text = textElements.innerText
   }
 
-  window.location.href = "https://translate.google.com/?q=" + encodeURIComponent(text);
+  window.location.href = 'https://translate.google.com/?q=' + encodeURIComponent(text)
 }
