283c6827128e9094784cad48abbff8724331f7db
danielzhang130 <danielzhang130@gmail.com>
2020-11-27 10:48:13 -0500

    Make code more testable

--- assets/js/custom/Translation.js
+++ assets/js/custom/Translation.js
@@ -1,16 +1,40 @@
-// eslint-disable-next-line no-unused-vars
-function Translation (textElements, srcLang) {
-  let text = ''
-  if (Array.isArray(textElements)) {
-    const array = []
-    for (let i = 0; i < textElements.length; i++) {
-      array.push(textElements[i].innerText)
-    }
+/* eslint-env jquery */
 
-    text = array.join('\n')
-  } else {
-    text = textElements.innerText
+function Translation(hasDescription, hasCredit) {
+  const elementsToTranslate = [document.getElementById('name')]
+
+  if (hasDescription) {
+    elementsToTranslate.push(document.getElementById('description'))
+  }
+  if (hasCredit) {
+    elementsToTranslate.push(document.getElementById('credits'))
   }
 
-  window.location.href = 'https://translate.google.com/?q=' + encodeURIComponent(text)
+  translation(
+    document.getElementById('googleTranslateLink'),
+    elementsToTranslate
+  )
+
+  $('.comment-translate-button').each(function() {
+      const commentId = $(this).attr('id').substring('comment-translate-button-'.length)
+      translation(this, document.getElementById("comment-text-" + commentId))
+    }
+  )
+
+  function translation (buttonElement, textElements) {
+    let text = ''
+    if (Array.isArray(textElements)) {
+      const array = []
+      for (let i = 0; i < textElements.length; i++) {
+        array.push(textElements[i].innerText)
+      }
+
+      text = array.join('\n')
+    } else {
+      text = textElements.innerText
+    }
+
+    buttonElement.setAttribute('href', 'https://translate.google.com/?q=' + encodeURIComponent(text))
+  }
+
 }
