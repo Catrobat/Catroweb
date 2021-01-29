ec20faf75bb0064cf3846362345e0784a1bfef13
Daniel Metzner <metzner@student.tugraz.at>
2021-01-29 16:42:02 +0100

    Bump Capi version and add default implementation

--- src/Api/UtilityApi.php
+++ src/Api/UtilityApi.php
@@ -2,7 +2,9 @@
 
 namespace App\Api;
 
+use OpenAPI\Server\Api\OpenAPI;
 use OpenAPI\Server\Api\UtilityApiInterface;
+use OpenAPI\Server\Model\SurveyResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class UtilityApi implements UtilityApiInterface
@@ -15,5 +17,13 @@ class UtilityApi implements UtilityApiInterface
     $responseCode = Response::HTTP_NO_CONTENT;
 
     return null;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function surveyLangCodeGet(string $lang_code, &$responseCode, array &$responseHeaders)
+  {
+    return new SurveyResponse(["url" => "https://www.surveylegend.com/s/2yaq"]);
   }
 }
