02cb1a6d57ad7f3083b900a48905d72eb3812810
Max Fazeli <m.fazeli@student.tugraz.at>
2021-05-02 11:30:41 +0200

    SHARE-419 Tabwidth in Userview should not change size
    For every tab in the Userview the number of projects, followers and following is now placed above the string and not on the left of the string.

--- public/build/request.js
+++ public/build/request.js
@@ -16,5 +16,11 @@ var usernameTextField = new _material_textfield__WEBPACK_IMPORTED_MODULE_0__.MDC
 /***/ })
 
 },
-0,[["./assets/js/material/request.js","runtime","vendors-node_modules_material_floating-label_component_js-node_modules_material_line-ripple_c-f46434","vendors-node_modules_material_textfield_component_js"]]]);
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = __webpack_require__.O(undefined, ["vendors-node_modules_material_ripple_component_js","vendors-node_modules_material_floating-label_component_js-node_modules_material_line-ripple_c-3efcfc","vendors-node_modules_material_textfield_component_js"], () => (__webpack_exec__("./assets/js/material/request.js")));
+/******/ }
+]);
 //# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9jYXRyb3dlYi8uL2Fzc2V0cy9qcy9tYXRlcmlhbC9yZXF1ZXN0LmpzIl0sIm5hbWVzIjpbInVzZXJuYW1lVGV4dEZpZWxkIiwiTURDVGV4dEZpZWxkIiwiZG9jdW1lbnQiLCJxdWVyeVNlbGVjdG9yIl0sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7OztDQUVBOztBQUNBLElBQU1BLGlCQUFpQixHQUFHLElBQUlDLDZEQUFKLENBQWlCQyxRQUFRLENBQUNDLGFBQVQsQ0FBdUIsb0JBQXZCLENBQWpCLENBQTFCLEMiLCJmaWxlIjoicmVxdWVzdC5qcyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IE1EQ1RleHRGaWVsZCB9IGZyb20gJ0BtYXRlcmlhbC90ZXh0ZmllbGQnXG5cbi8vIGVzbGludC1kaXNhYmxlLW5leHQtbGluZSBuby11bnVzZWQtdmFyc1xuY29uc3QgdXNlcm5hbWVUZXh0RmllbGQgPSBuZXcgTURDVGV4dEZpZWxkKGRvY3VtZW50LnF1ZXJ5U2VsZWN0b3IoJy51c2VybmFtZS1vci1lbWFpbCcpKVxuIl0sInNvdXJjZVJvb3QiOiIifQ==
