11b108c22a6f919388f9bdff7c6e2d5c8b22f2bb
joshschaefer <je2schae@uwaterloo.ca>
2021-07-02 01:36:02 -0400

    - Add API call when translate button is selected in program section - Replace title, description, and credits if available with translated text - Display translated by line with translation provider, source language and target language - If API fails, the user will be redirected to google translate

---
+++ assets/js/custom/TranslateComments.js
@@ -0,0 +1,79 @@
+/* eslint-env jquery */
+/* global Translation */
+/* global ByLineElementContainer */
+
+// eslint-disable-next-line no-unused-vars
+class TranslateComments extends Translation {
+  constructor (translatedByLine) {
+    super(translatedByLine)
+    this._initListeners()
+  }
+
+  _initListeners () {
+    const translateComments = this
+    $(document).on('click', '.comment-translation-button', function () {
+      const commentId = $(this).attr('id').substring('comment-translation-button-'.length)
+
+      $(this).hide()
+
+      if (translateComments.isTranslationNotAvailable('#comment-text-translation-' + commentId)) {
+        $('#comment-translation-loading-spinner-' + commentId).show()
+        translateComments.translateComment(commentId)
+      } else {
+        translateComments.openTranslatedComment(commentId)
+      }
+    })
+
+    $(document).on('click', '.remove-comment-translation-button', function () {
+      const commentId = $(this).attr('id').substring('remove-comment-translation-button-'.length)
+      $(this).hide()
+      $('#comment-translation-button-' + commentId).show()
+      $('#comment-translation-wrapper-' + commentId).slideUp()
+      $('#comment-text-wrapper-' + commentId).slideDown()
+    })
+  }
+
+  setTranslatedCommentData (commentId, data) {
+    $('#comment-text-translation-' + commentId).text(data.translation)
+    $('#comment-text-translation-' + commentId).attr('lang', data.target_language)
+
+    const byLineElements = new ByLineElementContainer(
+      '#comment-translation-before-languages-' + commentId,
+      '#comment-translation-between-languages-' + commentId,
+      '#comment-translation-after-languages-' + commentId,
+      '#comment-translation-first-language-' + commentId,
+      '#comment-translation-second-language-' + commentId
+    )
+
+    this.setTranslationCredit(data, byLineElements)
+  }
+
+  openTranslatedComment (commentId) {
+    $('#comment-translation-loading-spinner-' + commentId).hide()
+    $('#remove-comment-translation-button-' + commentId).show()
+    $('#comment-translation-wrapper-' + commentId).slideDown()
+    $('#comment-text-wrapper-' + commentId).slideUp()
+  }
+
+  commentNotTranslated (commentId) {
+    $('#comment-translation-loading-spinner-' + commentId).hide()
+    $('#comment-translation-button-' + commentId).show()
+    this.openGoogleTranslatePage(document.getElementById('comment-text-' + commentId).innerText)
+  }
+
+  translateComment (commentId) {
+    const self = this
+    $.ajax({
+      url: '../translate/comment/' + commentId,
+      type: 'get',
+      data: { target_language: self.targetLanguage },
+      success: function (data) {
+        self.setTranslatedCommentData(commentId, data)
+        self.openTranslatedComment(commentId)
+      },
+      error: function () {
+        self.commentNotTranslated(commentId)
+      }
+    })
+  }
+}
