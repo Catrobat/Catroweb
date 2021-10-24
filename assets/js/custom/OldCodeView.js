d157b6968dda3e0c8160901f81386bb82dca3ee8
Daniel Metzner <metzner@student.tugraz.at>
2021-10-24 21:58:30 +0200

    Hotfix catblocks

--- assets/js/custom/OldCodeView.js
+++
@@ -1,8 +0,0 @@
-/* eslint-env jquery */
-
-$(document).ready(function () {
-  $('.collapse-btn').on('click', function () {
-    $(this).next().slideToggle(250, 'linear')
-    $(this).find('.arrow').toggleClass('rotate')
-  })
-})
