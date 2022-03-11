002859b61951da78486583505e4370284aeb7fc8
Alexander Kropiunig <alexander.kropiunig@student.tugraz.at>
2022-03-11 14:45:23 +0100

    SHARE-478 GET /notifications/count
    -implemented notification/count API
    -removed old API
    -replaced usages of old API with new API
    -wrote test for API

--- src/Application/Controller/User/NotificationsController.php
+++ src/Application/Controller/User/NotificationsController.php
@@ -12,10 +12,10 @@ use App\DB\EntityRepository\User\Notification\NotificationRepository;
 use App\Project\Remix\RemixManager;
 use App\User\Notification\NotificationManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class NotificationsController extends AbstractController
