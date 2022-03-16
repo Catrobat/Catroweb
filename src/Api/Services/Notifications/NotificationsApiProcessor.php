bfb612b31c299d9f92639f9de821b12df256a7c0
Alexander Kropiunig <alexander.kropiunig@student.tugraz.at>
2022-03-16 11:00:09 +0100

    SHARE-479 PUT /notification/{id}/read
    -implemented /notification/{id}/read API
    -removed old API
    -wrote behat tests for API

--- src/Api/Services/Notifications/NotificationsApiProcessor.php
+++ src/Api/Services/Notifications/NotificationsApiProcessor.php
@@ -5,22 +5,20 @@ namespace App\Api\Services\Notifications;
 use App\Api\Services\Base\AbstractApiProcessor;
 use App\DB\Entity\User\User;
 use App\DB\EntityRepository\User\Notification\NotificationRepository;
-use App\Project\Remix\RemixManager;
 use App\User\Notification\NotificationManager;
 use Exception;
 
 final class NotificationsApiProcessor extends AbstractApiProcessor
 {
-  private RemixManager $remix_manager;
+  private NotificationRepository $notification_repository;
+  private NotificationManager $notification_manager;
 
   public function __construct(
         NotificationRepository $notification_repository,
         NotificationManager $notification_manager,
-        RemixManager $remix_manager
     ) {
     $this->notification_repository = $notification_repository;
     $this->notification_manager = $notification_manager;
-    $this->remix_manager = $remix_manager;
   }
 
   public function markNotificationAsSeen(int $notification_id, User $user): bool
