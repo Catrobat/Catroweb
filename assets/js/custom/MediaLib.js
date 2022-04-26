e526b998db8a0e54fd3e2126a0435cd6041e7d4a
Stephan Schauberger <stephan.schauberger@gmail.com>
2022-04-26 13:34:08 +0200

    fix media library search

--- assets/js/custom/MediaLib.js
+++ assets/js/custom/MediaLib.js
@@ -35,6 +35,15 @@ export function MediaLib (packageName, mediaSearchPath, flavor, assetsDir,
       showTopBarDefault()
     }
 
+    // accepted flavors
+    const acceptedFlavors = [
+      'pocketcode'
+    ]
+    if ($.inArray(flavor, acceptedFlavors) === -1) {
+      acceptedFlavors.push(flavor)
+    }
+
+    // url
     let url
     if (mediaSearchPath !== '') {
       url = mediaSearchPath
@@ -43,11 +52,23 @@ export function MediaLib (packageName, mediaSearchPath, flavor, assetsDir,
     }
 
     $.get(url, {}, pkgFiles => {
+      console.log(pkgFiles.length)
+
       pkgFiles.forEach(file => {
-        if (file.flavor !== 'pocketcode' && file.flavor !== flavor) {
-          return // don't display files of other flavors
+        // flavors
+        let fileFlavorArray = []
+        if ('flavors' in file && Array.isArray(file.flavors)) {
+          fileFlavorArray = file.flavors
+        } else if ('flavor' in file) {
+          fileFlavorArray.push(file.flavor)
         }
 
+        const flavorFound = fileFlavorArray.some(ai => acceptedFlavors.includes(ai))
+        if (!flavorFound) {
+          return
+        }
+
+        // media container
         const mediafileContainer = $('<a class="mediafile" id="mediafile-' + file.id + '"/>')
         mediafileContainer.click(function () {
           // !!! Due to missing android web view support the download multiple files feature was "disabled" !!!!!
@@ -87,13 +108,14 @@ export function MediaLib (packageName, mediaSearchPath, flavor, assetsDir,
           }
         })
 
-        if (flavor !== 'pocketcode' && file.flavor === flavor) {
+        const fileIsFlavored = !fileFlavorArray.includes('pocketcode')
+        if (fileIsFlavored) {
           mediafileContainer.addClass('flavored')
         }
 
         mediafileContainer.append($('<i class="checkbox material-icons">check_circle</i>'))
         mediafileContainer.append(buildImageContainer(file))
-        if (file.project_url) {
+        if ('project_url' in file && file.project_url) {
           mediafileContainer.append(
             '<div class="text-container">' +
             '  <a class="name name--link" href="' + file.project_url + '">' + getFileName(file) + '</a>' +
@@ -114,7 +136,7 @@ export function MediaLib (packageName, mediaSearchPath, flavor, assetsDir,
         }
 
         if (file.category.startsWith('ThemeSpecial')) {
-          if (file.flavor === flavor) {
+          if (flavorFound) {
             $('#content #category-theme-special .files').prepend(mediafileContainer)
           } // else ignore
         } else {
@@ -152,36 +174,32 @@ export function MediaLib (packageName, mediaSearchPath, flavor, assetsDir,
     const imageContainer = $('<div class="img-container"></div>')
     let audio, previewBtn
 
-    switch (file.type) {
-      case 'image':
-      case 'catrobat':
-        imageContainer.attr('data-filetype', 'image')
-        imageContainer.append(buildImageFromFile(file))
-        break
-      case 'sound':
-        imageContainer.attr('data-filetype', 'audio')
-        audio = new Audio(file.download_url)
-        previewBtn = $('<i class="audio-control material-icons">play_arrow</i>')
-        previewBtn.click(function () {
-          if (audio.paused) {
-            previewBtn.text('pause')
-            audio.play()
-          } else {
-            previewBtn.text('play_arrow')
-            audio.pause()
-          }
-          return false
-        })
-        audio.onended = function () {
+    if (isFileExtAnImageFile(file.extension) || file.extension === 'catrobat') {
+      imageContainer.attr('data-filetype', 'image')
+      imageContainer.append(buildImageFromFile(file))
+    } else if (isFileExtASoundFile(file.extension)) {
+      imageContainer.attr('data-filetype', 'audio')
+      // eslint-disable-next-line no-undef
+      audio = new Audio(file.download_url)
+      previewBtn = $('<i class="audio-control material-icons">play_arrow</i>')
+      previewBtn.click(function () {
+        if (audio.paused) {
+          previewBtn.text('pause')
+          audio.play()
+        } else {
           previewBtn.text('play_arrow')
+          audio.pause()
         }
-        imageContainer.append(previewBtn)
-        break
-      case 'video':
-        imageContainer.append($('<i class="media-file-icon material-icons">videocam</i>'))
-        break
-      default:
-        imageContainer.append($('<i class="media-file-icon material-icons">insert_drive_file</i>'))
+        return false
+      })
+      audio.onended = function () {
+        previewBtn.text('play_arrow')
+      }
+      imageContainer.append(previewBtn)
+    } else if (isFileExtAVideoFile(file.extension)) {
+      imageContainer.append($('<i class="media-file-icon material-icons">videocam</i>'))
+    } else {
+      imageContainer.append($('<i class="media-file-icon material-icons">insert_drive_file</i>'))
     }
 
     return imageContainer
@@ -291,4 +309,34 @@ function medialibOnDownload (link) {
     window.location = downloadHref
   }
   return false
+}
+
+/**
+ * file type checks
+ */
+function isFileExtASoundFile (ext) {
+  const fileTypes = [
+    'adp', 'au', 'mid', 'mp4a', 'mpga', 'oga', 's3m', 'sil', 'uva', 'eol', 'dra', 'dts', 'dtshd', 'lvp', 'pya',
+    'ecelp4800', 'ecelp7470', 'ecelp9600', 'rip', 'weba', 'aac', 'aif', 'caf', 'flac', 'mka', 'm3u', 'wax', 'wma',
+    'ram', 'rmp', 'wav', 'xm'
+  ]
+  return fileTypes.includes(ext)
+}
+
+function isFileExtAnImageFile (ext) {
+  const fileTypes = [
+    'bmp', 'cgm', 'g3', 'gif', 'ief', 'jpeg', 'ktx', 'png', 'btif', 'sgi', 'svg', 'tiff', 'psd', 'uvi', 'sub', 'djvu',
+    'dwg', 'dxf', 'fbs', 'fpx', 'fst', 'mmr', 'rlc', 'mdi', 'wdp', 'npx', 'wbmp', 'xif', 'webp', '3ds', 'ras', 'cmx',
+    'fh', 'ico', 'sid', 'pcx', 'pic', 'pnm', 'pbm', 'pgm', 'ppm', 'rgb', 'tga', 'xbm', 'xpm', 'xwd'
+  ]
+  return fileTypes.includes(ext)
+}
+
+function isFileExtAVideoFile (ext) {
+  const fileTypes = [
+    '3gp', '3g2', 'h261', 'h263', 'h264', 'jpgv', 'jpm', 'mj2', 'mp4', 'mpeg', 'ogv', 'qt', 'uvh', 'uvm', 'uvp',
+    'uvs', 'uvv', 'dvb', 'fvt', 'mxu', 'pyv', 'uvu', 'viv', 'webm', 'f4v', 'fli', 'flv', 'm4v', 'mkv', 'mng', 'asf',
+    'vob', 'wm', 'wmv', 'wmx', 'wvx', 'avi', 'movie', 'smv'
+  ]
+  return fileTypes.includes(ext)
 }
