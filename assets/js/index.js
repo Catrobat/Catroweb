b3d3a23e96587613f074a096aa55618f82ba44ed
Daniel Metzner <daniel.metzner@niceshops.com>
2023-02-17 12:23:09 +0100

    Hotfix: Remove jquery from start page & prevent unnecessary 2nd query for oauth greeting

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,75 +1,48 @@
-import $ from 'jquery'
 import { Carousel } from 'bootstrap'
-import Swal from 'sweetalert2'
 import { ProjectList } from './components/project_list'
+import { OAuthHandler } from './security/OAuthHandler'
 
 require('../styles/index.scss')
 
-$(() => {
+document.addEventListener('DOMContentLoaded', () => {
   initFeatureSlider()
   initHomeProjects()
-  initOauthGreeting()
+  const oAuthHandler = new OAuthHandler()
+  oAuthHandler.showOAuthFirstLoginInformationIfNecessary()
 })
 
 function initFeatureSlider () {
-  if ($('#feature-slider').length > 0) {
-    new Carousel('#feature-slider')
+  const featureSlider = document.getElementById('feature-slider')
+  if (featureSlider) {
+    new Carousel(featureSlider)
   } else {
-    console.warn("#feature-slider can't be found in the dom.")
+    console.warn('#feature-slider can\'t be found in the dom.')
   }
 }
 
 function initHomeProjects () {
-  const $homeProjects = $('#home-projects')
-  $('.project-list', $homeProjects).each(function () {
-    const category = $(this).data('category')
-    const property = $(this).data('property')
-    const theme = $(this).data('theme')
-    const flavor = $(this).data('flavor')
-    const baseUrl = $(this).data('base-url')
+  const homeProjects = document.getElementById('home-projects')
+  const projectLists = homeProjects.querySelectorAll('.project-list')
 
-    let url = baseUrl + '/api/projects?category=' + category
+  projectLists.forEach((projectList) => {
+    const category = projectList.dataset.category
+    const property = projectList.dataset.property
+    const theme = projectList.dataset.theme
+    const flavor = projectList.dataset.flavor
+    const baseUrl = projectList.dataset.baseUrl
+
+    let url = `${baseUrl}/api/projects?category=${category}`
 
     if (flavor !== 'pocketcode' || category === 'example') {
-      // Only the pocketcode flavor shows projects from all flavors!
-      // Other flavors must only show projects from their flavor.
-      url += '&flavor=' + flavor
+      url += `&flavor=${flavor}`
     }
 
-    const list = new ProjectList(this, category, url, property, theme)
-    $(this).data('list', list)
-  })
-}
-
-function initOauthGreeting () {
-  const $oauthGreeting = $('.js-oauth-greeting')
-  showOauthPopup(
-    $oauthGreeting.data('path-oauth-first-login'),
-    $oauthGreeting.data('trans-info'),
-    $oauthGreeting.data('trans-title'),
-    $oauthGreeting.data('trans-ok')
-  )
-}
-
-function showOauthPopup (firstOauthLoginUrl, informationText, title, okTranslation) {
-  $.get(firstOauthLoginUrl, function (data) {
-    if (data.first_login === true) {
-      const shown = localStorage.getItem('oauthSignIn')
-      if (shown == null) {
-        localStorage.setItem('oauthSignIn', '1')
-        Swal.fire({
-          title,
-          html: informationText,
-          showCancelButton: false,
-          allowOutsideClick: false,
-          confirmButtonText: okTranslation,
-          icon: 'info',
-          customClass: {
-            confirmButton: 'btn btn-primary'
-          },
-          buttonsStyling: false
-        })
-      }
-    }
+    projectList.dataset.list = new ProjectList(
+      projectList,
+      category,
+      url,
+      property,
+      theme
+    ).toString()
   })
 }
