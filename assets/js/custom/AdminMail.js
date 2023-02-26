911e5ac6a34f15befd276fdf5e338aae58112d3a
David Url <david.url@student.tugraz.at>
2023-02-26 15:23:48 +0100

    SHARE-533 Email templates & preview

--- assets/js/custom/AdminMail.js
+++ assets/js/custom/AdminMail.js
@@ -2,14 +2,15 @@
 
 // eslint-disable-next-line no-unused-vars
 function AdminMail () {
-  $('.btn').click(function () {
+  $('.btn-send').click(function () {
     const username = $('#username').val()
     const subject = $('#subject').val()
+    const titel = $('titel').val()
     const message = $('#content').val()
     const resultBox = $('.resultBox')
 
     // calls _/Controller/Admin/Tools/SendMailToUserController::sendAction
-    $.get('send', { username, subject, message }, function (data) {
+    $.get('send', { username, subject, titel, message }, function (data) {
       if (data && data.length >= 2 && data.substring(0, 2) === 'OK') {
         resultBox.removeClass('error')
         resultBox.addClass('success')
@@ -19,5 +20,16 @@ function AdminMail () {
       }
       resultBox.html(data)
     })
+  })
+
+  // calls _/Controller/Admin/Tools/SendMailToUserController::previewAction
+  $('.btn-preview').click(function () {
+    const username = $('#username').val()
+    const subject = $('#subject').val()
+    const titel = $('#titel').val()
+    const message = $('#content').val()
+    const template = $('#template-select').val()
+    const url = 'preview?username=' + username + '&subject=' + subject + '&titel=' + titel + '&message=' + message + '&template=' + template
+    window.open(url, '_blank')
   })
 }
