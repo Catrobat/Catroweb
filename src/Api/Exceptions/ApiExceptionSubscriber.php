f5323181c7e32e0c6ac78ef34f51df40ea600db0
David Url <david.url@student.tugraz.at>
2023-03-18 06:44:18 +0100

    SHARE-543 Project download as non-user throws ugly error

--- src/Api/Exceptions/ApiExceptionSubscriber.php
+++ src/Api/Exceptions/ApiExceptionSubscriber.php
@@ -5,9 +5,8 @@ namespace App\Api\Exceptions;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
-use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
-
+use Symfony\Component\HttpKernel\KernelEvents;
 
 class ApiExceptionSubscriber implements EventSubscriberInterface
 {
