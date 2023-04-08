98495ce88d136337b5c1a80ece5dbf870cf5bcaa
David Url <david.url@student.tugraz.at>
2023-04-08 05:59:23 +0200

    SHARE-524 Feedback for password reset attempts

--- config/services.php
+++ config/services.php
@@ -201,6 +201,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
   $parameters->set('es_port', '%env(ES_PORT)%');
   $parameters->set('dkim.private.key', '%kernel.project_dir%/.dkim/private.key');
   $parameters->set('container.dumper.inline_class_loader', true);
+  $parameters->set('reset_password.throttle_limit', 86400);
 
   $services = $containerConfigurator->services();
 
