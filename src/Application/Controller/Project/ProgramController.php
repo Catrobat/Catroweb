261fa0d5b5f02f51b97648a3dcfa3e90c54bcebf
Markus Kreuzer <kreuzer@student.tugarz.at>
2023-09-02 11:53:39 +0200

    SHARE-557 Introduction Task
    Resolve coding style issues and fix user casting in ProgramController

--- src/Application/Controller/Project/ProgramController.php
+++ src/Application/Controller/Project/ProgramController.php
@@ -120,7 +120,6 @@ class ProgramController extends AbstractController
     }
     /** @var User|null $user */
     $user = $this->getUser();
-    $user = $this->getUser();
     $logged_in = null !== $user;
     $this->addFlash('snackbar', 'This project has been successfully stolen');
     $project->setUser($user);
