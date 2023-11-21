10af71f06a8c9c0c048a69a86dcd55a00623cd7a
Markus Kreuzer <kreuzer@student.tugarz.at>
2023-11-21 22:15:34 +0100

    Merge branch 'Catrobat:develop' into SHARE-554

--- src/System/Testing/Behat/Context/CatrowebBrowserContext.php
+++ src/System/Testing/Behat/Context/CatrowebBrowserContext.php
@@ -1380,6 +1380,25 @@ class CatrowebBrowserContext extends BrowserContext
       $this->assertSession()->pageTextContains($user_stat['Apk Request Time']);
     }
   }
+   /**
+    * @Then /^I should see the ready maintenance information table:$/
+    *
+    * @throws ResponseTextException
+    */
+    public function seeReadyMaintenanceInformationTable(TableNode $table): void
+    {
+        $user_stats = $table->getHash();
+        foreach ($user_stats as $user_stat) {
+            $this->assertSession()->pageTextContains($user_stat['Feature Name']);
+            $this->assertSession()->pageTextContains($user_stat['Active']);
+            $this->assertSession()->pageTextContains($user_stat['LTM Code']);
+            $this->assertSession()->pageTextContains($user_stat['Maintenance Start']);
+            $this->assertSession()->pageTextContains($user_stat['Maintenance End']);
+            $this->assertSession()->pageTextContains($user_stat['Additional Information']);
+            $this->assertSession()->pageTextContains($user_stat['Icon']);
+            $this->assertSession()->pageTextContains($user_stat['Actions']);
+        }
+    }
 
   /**
    * @Then /^I should see the pending apk table:$/
