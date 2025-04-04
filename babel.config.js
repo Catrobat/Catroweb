84536985b7cdb4cd0b63645594b7740ece6e603d
Daniel Metzner <daniel.metzner@niceshops.com>
2025-04-04 12:39:45 +0200

    Global babel config
    Signed-off-by: Daniel Metzner <daniel.metzner@niceshops.com>

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,14 @@
 module.exports = {
-  presets: ['@babel/preset-env'],
-  // Add other presets or plugins as needed
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'usage', // Only includes the polyfills you need based on the usage
+        corejs: 3, // Use core-js version 3 for polyfilling
+      },
+    ],
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties', // Add the class properties plugin
+  ],
 }
--- webpack.config.js
+++ webpack.config.js
@@ -162,15 +162,7 @@ Encore
   // enables hashed filenames (e.g. app.abc123.css)
   .enableVersioning(Encore.isProduction())
 
-  .configureBabel((config) => {
-    config.plugins.push('@babel/plugin-proposal-class-properties')
-  })
-
-  // enables @babel/preset-env polyfills
-  .configureBabelPresetEnv((config) => {
-    config.useBuiltIns = 'usage'
-    config.corejs = 3
-  })
+  // Babel configured via babel.config.js instead of Encore
 
   // enables Sass/SCSS support
   .enableSassLoader()
