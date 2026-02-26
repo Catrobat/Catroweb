27c1302d2fa9facfcde9f7141628a10e76a8d6d2
Daniel Metzner <daniel.metzner@niceshops.com>
2026-02-26 14:33:01 +0100

    EsLint 10 Upgrade

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,8 @@ const { defineConfig, globalIgnores } = require('eslint/config')
 const globals = require('globals')
 const js = require('@eslint/js')
 const prettierPlugin = require('eslint-plugin-prettier/recommended')
-const importPlugin = require('eslint-plugin-import')
+const importPlugin = require('eslint-plugin-import-x')
 // const pluginPromise = require('eslint-plugin-promise')
-const babelParser = require('@babel/eslint-parser') // Import the parser object
 
 module.exports = defineConfig([
   globalIgnores([
@@ -19,13 +18,9 @@ module.exports = defineConfig([
     files: ['**/*.js'],
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
-      parser: babelParser, // Use the imported parser object
       parserOptions: {
         ecmaVersion: 'latest',
-        sourceType: 'module', // Adjust if your files are not all ES modules
-        babelOptions: {
-          presets: ['@babel/preset-env'], // Optional: If you use Babel presets
-        },
+        sourceType: 'module',
       },
     },
   },
